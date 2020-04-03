d1608ee94b0b6fb8509acbe792251e4da6ee8724
fclh <fclh1991@gmail.com>
2020-04-03 17:44:55 +0800

    remove the useless hdfs call (#30)
    Co-authored-by: fangchenliaohui <fangchenliaohui@bytedance.com>

--- fedlearner/data_join/data_block_visitor.py
+++ fedlearner/data_join/data_block_visitor.py
@@ -28,7 +28,7 @@ from fedlearner.data_join.common import (
 
 class DataBlockRep(object):
     def __init__(self, data_source_name, data_block_fname,
-                 partition_id, dirpath):
+                 partition_id, dirpath, check_existed=True):
         assert data_block_fname.endswith(DataBlockSuffix), \
             "data block fname {} should has suffix {}".format(
                 data_block_fname, DataBlockSuffix
@@ -58,7 +58,8 @@ class DataBlockRep(object):
                                                   self._partition_id,
                                                   self._data_block_index)
         meta_fpath = os.path.join(dirpath, meta_fname)
-        if not gfile.Exists(meta_fpath) or gfile.IsDirectory(meta_fpath):
+        if check_existed and (not gfile.Exists(meta_fpath) or \
+                              gfile.IsDirectory(meta_fpath)):
             raise ValueError("{} invalid. the corresponding meta file "\
                              "is not existed".format(data_block_fname))
         self._data_block_meta_fpath = meta_fpath
@@ -107,13 +108,16 @@ class DataBlockVisitor(object):
                 start_time > self._data_source.data_source_meta.end_time:
             raise ValueError("time frame is out of range")
         partition_num = self._data_source.data_source_meta.partition_num
-        data_block_fnames = []
+        data_block_fnames = {}
         for partition_id in range(0, partition_num):
-            data_block_fnames.append(self._list_data_block(partition_id))
+            data_block_fnames[partition_id] = \
+                self._list_data_block(partition_id)
         data_block_reps = {}
-        for partition_id, fnames in enumerate(data_block_fnames):
-            for fname in fnames:
-                rep = self._make_data_block_rep(partition_id, fname)
+        for partition_id, fnames in data_block_fnames.items():
+            for idx, fname in enumerate(fnames):
+                check_existed = (idx == len(fnames) - 1)
+                rep = self._make_data_block_rep(partition_id, fname,
+                                                check_existed)
                 if rep is not None and\
                         not (rep.start_time > end_time or\
                                 rep.end_time < start_time):
@@ -140,19 +144,20 @@ class DataBlockVisitor(object):
         dirpath = self._partition_data_block_dir(partition_id)
         if gfile.Exists(dirpath) and gfile.IsDirectory(dirpath):
             return [f for f in gfile.ListDirectory(dirpath)
-                    if (not gfile.IsDirectory(os.path.join(dirpath, f)) and
-                        f.endswith(DataBlockSuffix))]
+                    if f.endswith(DataBlockSuffix)]
         return []
 
     def _partition_data_block_dir(self, partition_id):
         return os.path.join(self._data_source.data_block_dir,
                             partition_repr(partition_id))
 
-    def _make_data_block_rep(self, partition_id, data_block_fname):
+    def _make_data_block_rep(self, partition_id,
+                             data_block_fname, check_existed):
         try:
             rep = DataBlockRep(self._data_source.data_source_meta.name,
                                data_block_fname, partition_id,
-                               self._partition_data_block_dir(partition_id))
+                               self._partition_data_block_dir(partition_id),
+                               check_existed)
         except Exception as e: # pylint: disable=broad-except
             logging.warning("Failed to create data block rep for %s in"\
                             "partition %d reason %s", data_block_fname,
