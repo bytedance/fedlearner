39fd5c9267fe045049b2e270420cb8e574a03c89
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2020-04-28 15:57:50 +0800

    Fix nan handling and add logging (#74)
    * restructure test
    
    * fix
    
    * fix nan handling
    
    * add logging
    
    * fix

--- test/trainer/disabled_bridge.py
+++ test/trainer/disabled_bridge.py
@@ -19,7 +19,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import unittest
 import threading
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import numpy as np
 
 import fedlearner as fl
