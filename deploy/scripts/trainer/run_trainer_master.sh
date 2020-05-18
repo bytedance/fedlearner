6451644da19b8209884b1cf1f2bc6e3a33cc937d
fclh <fclh1991@gmail.com>
2020-05-18 21:44:29 +0800

    add the common hdfs config for deploy script (#111)
    Co-authored-by: fangchenliaohui <fangchenliaohui@bytedance.com>

--- deploy/scripts/trainer/run_trainer_master.sh
+++ deploy/scripts/trainer/run_trainer_master.sh
@@ -17,6 +17,8 @@
 set -ex
 
 export CUDA_VISIBLE_DEVICES=
+export LD_LIBRARY_PATH=${HADOOP_HOME}/lib/native:${JAVA_HOME}/jre/lib/amd64/server:${LD_LIBRARY_PATH}
+export CLASSPATH=.:$CLASSPATH:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$($HADOOP_HOME/bin/hadoop classpath --glob)
 
 python -m fedlearner.trainer_master.${ROLE}_tm \
     --app-id=$APPLICATION_ID \
