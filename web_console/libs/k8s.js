a2f5466606f59d31e660574bbdf83c4428f0f258
Mars Wong <marswong618@gmail.com>
2020-06-15 20:44:39 +0800

    perf(k8s): testing with encapsulated server instead of native server

--- web_console/libs/k8s.js
+++ web_console/libs/k8s.js
@@ -1,26 +1,30 @@
 // Kubernetes Client
 const got = require('got');
 
+let config;
+try {
+  config = require('../server.config');
+} catch (err) {
+  config = require('../constants').DEFAULT_SERVER_CONFIG;
+}
+
 class KubernetesClient {
-  constructor(options) {
+  constructor() {
     // TODO: use HTTPs for production
-    if (options.proxy) {
-      this.client = got.extend({
-        prefixUrl: `${options.proxy}/api/v1`,
-        responseType: 'json',
-      });
-    }
+    const prefixUrl = `http://${process.env.K8S_HOST || config.K8S_HOST}:${process.env.K8S_PORT || config.K8S_PORT}`;
+    this.client = got.extend({
+      prefixUrl,
+      responseType: 'json',
+    });
   }
 
-  // TODO: apply filters
   async getNamespaces() {
     const { body } = await this.client.get('namespaces');
     return body;
   }
 
-  // TODO: apply filters
-  async getPods() {
-    const { body } = await this.client.get('pods');
+  async getFLAppsByNamespace(namespace) {
+    const { body } = await this.client.get(`namespaces/${namespace}/fedlearner/v1alpha1/flapps`);
     return body;
   }
 }
