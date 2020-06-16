e341c359d8a744b03b659bb2fb6a418a23db6a9e
rapmetal <yijia.lilong@gmail.com>
2020-06-16 15:32:50 +0800

    update k8s_client function

--- web_console/libs/k8s.js
+++ web_console/libs/k8s.js
@@ -6,23 +6,47 @@ class KubernetesClient {
     // TODO: use HTTPs for production
     if (options.proxy) {
       this.client = got.extend({
-        prefixUrl: `${options.proxy}/api/v1`,
+        prefixUrl: `${options.proxy}/api/v1/`,
         responseType: 'json',
       });
     }
   }
 
-  // TODO: apply filters
   async getNamespaces() {
     const { body } = await this.client.get('namespaces');
     return body;
   }
 
-  // TODO: apply filters
-  async getPods() {
-    const { body } = await this.client.get('pods');
+  async getFLAppsByNamespace(namespace) {
+    const { body } = await this.client.get(`namespaces/${namespace}/fedlearner/v1alpha1/flapps`);
     return body;
   }
+
+  async getFLApps(namespace, name) {
+    const { body } = await this.client.get(`namespaces/${namespace}/fedlearner/v1alpha1/flapps/${name}`);
+    return body;
+  }
+
+  async getFLApps(namespace, name) {
+    const { body } = await this.client.get(`namespaces/${namespace}/fedlearner/v1alpha1/flapps/${name}`);
+    return body;
+  }
+
+  async getFLAppPods(namespace, name) {
+    const { body } = await this.client.get(`/namespaces/${namespace}/fedlearner/v1alpha1/flapps/${name}/pods`);
+    return body;
+  }
+
+  async createFLApp(namespace, fl_app) {
+    const { body } = await this.client.post(`/namespaces/${namespace}/fedlearner/v1alpha1/flapps`, { body: fl_app });
+    return body;
+  }
+
+  async deleteFLApp(namespace) {
+    const { body } = await this.client.delete(`/namespaces/${namespace}/fedlearner/v1alpha1/flapps/${name}`);
+    return body;
+  }
+
 }
 
 module.exports = KubernetesClient;
