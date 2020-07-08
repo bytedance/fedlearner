4e84b7898c0e04157746cc64bf7ec4ee37aba320
codemonkey-ll <65523085+codemonkey-ll@users.noreply.github.com>
2020-07-08 13:45:13 +0800

    Implementation of data portal worker (#94)
    * data portal worker implementation
    
    * data portal worker, some issue fix
    
    * add _ for inner function

---
+++ fedlearner/data_join/merge.py
@@ -0,0 +1,226 @@
+# Copyright 2020 The FedLearner Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+
+
+import os
+import random
+import logging
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+from tensorflow.compat.v1 import gfile
+import tensorflow.compat.v1 as tf
+
+from fedlearner.data_join.raw_data_iter_impl.tf_record_iter import TfRecordIter
+from fedlearner.data_join import visitor, common
+
+class Merge(object):
+    class RecordItem(object):
+        def __init__(self, fpath_id, tf_example_item):
+            self._fpath_id = fpath_id
+            self._tf_example_item = tf_example_item
+
+        @property
+        def fpath_id(self):
+            return self._fpath_id
+
+        @property
+        def tf_example_item(self):
+            return self._tf_example_item
+
+        def __lt__(self, other):
+            assert isinstance(other, Merge.RecordItem)
+            return self._tf_example_item.event_time < \
+                    other.tf_example_item.event_time
+
+    class InputFileReader(object):
+        def __init__(self, fpath_id, fpath, options):
+            self._fpath_id = fpath_id
+            self._fpath = fpath
+            self._raw_data_options = options.raw_data_options
+            self._fiter = None
+            if gfile.Exists(fpath):
+                self._finished = False
+            else:
+                self._finished = True
+
+        @property
+        def finished(self):
+            return self._finished
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            return self._next_internal()
+
+        def next(self):
+            return self._next_internal()
+
+        def _next_internal(self):
+            if not self._finished:
+                try:
+                    while True:
+                        tf_item = None
+                        if self._fiter is None:
+                            self._fiter = TfRecordIter(self._raw_data_options)
+                            meta = visitor.IndexMeta(0, 0, self._fpath)
+                            self._fiter.reset_iter(meta, True)
+                            tf_item = self._fiter.get_item()
+                        else:
+                            _, tf_item = next(self._fiter)
+                        return Merge.RecordItem(self._fpath_id, tf_item)
+                except StopIteration:
+                    self._finished = True
+            raise StopIteration("%s has been iter finished" % self._fpath)
+
+    class FileMeta(object):
+        def __init__(self, partition_id, begin_index, end_index):
+            self._partition_id = partition_id
+            self._begin_index = begin_index
+            self._end_index = end_index
+
+        def encode_meta_to_fname(self):
+            return '{:04}.{:010}-{:010}{}'.format(
+                self._partition_id, self._begin_index,
+                self._end_index, common.RawDataFileSuffix)
+
+
+    class OutputFileWriter(object):
+        def __init__(self, options, partition_id):
+            self._partition_id = partition_id
+            self._writer = None
+            self._begin_index = None
+            self._end_index = None
+            self._options = options
+            self._size_bytes = 0
+            self._tmp_fpath = common.gen_tmp_fpath(
+                os.path.join(self._options.output_dir,
+                    common.partition_repr(self._partition_id))
+            )
+
+        def append_item(self, item, index):
+            writer = self._get_output_writer()
+            tf_item = item.tf_record
+            writer.write(tf_item)
+            if self._begin_index is None:
+                self._begin_index = index
+            self._end_index = index
+            self._size_bytes += len(tf_item)
+            if self._size_bytes >= self._options.output_item_threshold:
+                writer.close()
+                self.writer = None
+                meta = Merge.FileMeta(self._partition_id, self._begin_index,
+                    self._end_index)
+                fpath = os.path.join(self._options.output_dir,
+                    common.partition_repr(self._partition_id),
+                    meta.encode_meta_to_fname())
+                gfile.Rename(self.get_tmp_fpath(), fpath, True)
+                self._size_bytes = 0
+                self._begin_index = None
+                self._end_index = None
+                self._writer = None
+
+        def finish(self):
+            if self._begin_index is not None \
+                and self._end_index is not None:
+                self._writer.close()
+                meta = Merge.FileMeta(self._partition_id, self._begin_index,
+                    self._end_index)
+                fpath = os.path.join(self._options.output_dir,
+                    common.partition_repr(self._partition_id),
+                    meta.encode_meta_to_fname())
+                gfile.Rename(self.get_tmp_fpath(), fpath, True)
+                self._writer = None
+
+        def get_tmp_fpath(self):
+            return self._tmp_fpath
+
+        def _get_output_writer(self):
+            if self._writer is None:
+                self._new_writer()
+            return self._writer
+
+        def _new_writer(self):
+            assert self._writer is None
+            self._writer = tf.io.TFRecordWriter(self._tmp_fpath)
+
+
+    def __init__(self, options, partition_id):
+        self._readers = []
+        self._options = options
+        self._partition_id = partition_id
+        self._queue = queue.PriorityQueue(options.merge_buffer_size)
+        self._active_fpath = set()
+        self._fpaths = gfile.ListDirectory(self._options.input_dir)
+        self._fpath_num = len(self._fpaths)
+        self._writer = Merge.OutputFileWriter(self._options, self._partition_id)
+        self._prepare()
+
+    def _prepare(self):
+        self._output_dir = os.path.join(self._options.output_dir,
+            common.partition_repr(self._partition_id))
+        if gfile.Exists(self._output_dir):
+            gfile.DeleteRecursively(self._output_dir)
+        gfile.MkDir(self._options.output_dir)
+        gfile.MkDir(self._output_dir)
+        for fpath_id, fpath in enumerate(self._fpaths):
+            fpath = "{}/{}".format(self._options.input_dir, fpath)
+            reader = Merge.InputFileReader(fpath_id,
+                fpath, self._options)
+            self._readers.append(reader)
+            self._active_fpath.add(fpath_id)
+            logging.info("Merge partition_id:%d, path:%s",
+                self._partition_id, fpath)
+        self._preload_queue()
+
+    def _preload_queue(self):
+        fpath_id = 0
+        size_active = len(self._active_fpath)
+        while not self._queue.full() and size_active > 0:
+            if fpath_id not in self._active_fpath:
+                continue
+            self._replenish_item(fpath_id)
+            fpath_id = (fpath_id + 1) % self._fpath_num
+
+    def _replenish_item(self, fpath_id):
+        assert not self._queue.full(), \
+            "Priority Queue should not full duration replenish_item"
+        while len(self._active_fpath) > 0:
+            if fpath_id in self._active_fpath:
+                for item in self._readers[fpath_id]:
+                    self._queue.put(item)
+                    return
+                assert self._readers[fpath_id].finished
+                self._active_fpath.discard(fpath_id)
+            if len(self._active_fpath) > 0:
+                fpath_id = random.choice(list(self._active_fpath))
+
+    def finish(self):
+        success_path = "{}/{}".format(self._output_dir, "_SUCCESS")
+        with gfile.GFile(success_path, 'w') as f:
+            f.write("")
+
+    def generate_output(self):
+        index = 0
+        while not self._queue.empty():
+            record_item = self._queue.get()
+            self._replenish_item(record_item.fpath_id)
+            self._writer.append_item(record_item.tf_example_item, index)
+            index += 1
+        self._writer.finish()
---
+++ test/data_join/test_data_portal_worker.py
@@ -0,0 +1,188 @@
+# Copyright 2020 The FedLearner Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+
+import os
+import random
+import unittest
+import logging
+
+from tensorflow.compat.v1 import gfile
+import tensorflow as tf
+
+from cityhash import CityHash32
+
+from fedlearner.common import data_join_service_pb2 as dj_pb
+from fedlearner.common import data_portal_service_pb2 as dp_pb
+from fedlearner.data_join.data_portal_worker import DataPortalWorker
+from fedlearner.data_join.raw_data_iter_impl.tf_record_iter import TfExampleItem
+from fedlearner.data_join import common
+
+class TestDataPortalWorker(unittest.TestCase):
+
+    def _get_input_fpath(self, partition_id):
+        return "{}/raw_data_partition_{}".format(self._input_dir, partition_id)
+
+    def _generate_one_partition(self, partition_id, example_id, num_examples):
+        fpath = self._get_input_fpath(partition_id)
+        with tf.io.TFRecordWriter(fpath) as writer:
+            for i in range(num_examples):
+                example_id += random.randint(1, 5)
+                # real_id = example_id.encode("utf-8")
+                event_time = 150000000 + random.randint(10000000, 20000000)
+                feat = {}
+                feat['example_id'] = tf.train.Feature(
+                    bytes_list=tf.train.BytesList(value=[str(example_id).encode('utf-8')]))
+                feat['raw_id'] = tf.train.Feature(
+                    bytes_list=tf.train.BytesList(value=[str(example_id).encode('utf-8')]))
+                feat['event_time'] = tf.train.Feature(
+                    int64_list=tf.train.Int64List(value=[event_time]))
+                example = tf.train.Example(features=tf.train.Features(feature=feat))
+                writer.write(example.SerializeToString())
+        return example_id
+
+
+    def _generate_input_data(self):
+        self._partition_item_num = 1 << 16
+        self._clean_up()
+        gfile.MakeDirs(self._input_dir)
+        success_flag_fpath = "{}/_SUCCESS".format(self._input_dir)
+        example_id = 1000001
+        for partition_id in range(self._input_partition_num):
+            example_id = self._generate_one_partition(partition_id, example_id, self._partition_item_num)
+        
+        with gfile.GFile(success_flag_fpath, 'w') as fh:
+            fh.write('')
+
+    def _make_portal_worker(self):
+        raw_data_options = dj_pb.RawDataOptions(
+            raw_data_iter="TF_RECORD",
+            compressed_type='')
+        batch_processor_options = dj_pb.BatchProcessorOptions(
+            batch_size=128,
+            max_flying_item=300000)
+        partitioner_options = dj_pb.RawDataPartitionerOptions(
+            raw_data_options=raw_data_options,
+            batch_processor_options=batch_processor_options,
+            output_item_threshold=10485760)
+        merge_options = dj_pb.MergeOptions(
+            raw_data_options=raw_data_options,
+            batch_processor_options=batch_processor_options,
+            merge_buffer_size=4096,
+            output_item_threshold=5000000)
+        
+        portal_worker_options = dj_pb.DataPortalWorkerOptions(
+            partitioner_options=partitioner_options,
+            merge_options=merge_options)
+
+        self._portal_worker = DataPortalWorker(portal_worker_options,
+            "localhost:5005", 0, "test_portal_worker_0",
+            "portal_worker_0", "localhost:2379", True)
+
+    def _clean_up(self):
+        if gfile.Exists(self._input_dir):
+            gfile.DeleteRecursively(self._input_dir)
+        if gfile.Exists(self._partition_output_dir):
+            gfile.DeleteRecursively(self._partition_output_dir)
+        if gfile.Exists(self._merge_output_dir):
+            gfile.DeleteRecursively(self._merge_output_dir)
+
+    def _prepare_test(self):
+        self._input_dir = './portal_worker_input'
+        self._partition_output_dir = './portal_worker_partition_output'
+        self._merge_output_dir = './portal_worker_merge_output'
+        self._input_partition_num = 4
+        self._output_partition_num = 2
+        self._generate_input_data()
+        self._make_portal_worker()
+
+    def _check_partitioner(self, map_task):
+        output_partitions = gfile.ListDirectory(map_task.output_base_dir)
+        output_partitions = [x for x in output_partitions if "SUCCESS" not in x]
+        self.assertEqual(len(output_partitions), map_task.output_partition_num)
+        partition_dirs = ["{}/{}".format(map_task.output_base_dir, x) \
+            for x in output_partitions]
+        
+        total_cnt = 0
+        for partition in output_partitions:
+            dpath = "{}/{}".format(map_task.output_base_dir, partition)
+            partition_id = partition.split("_")[-1]
+            partition_id = int(partition_id)
+            segments = gfile.ListDirectory(dpath)
+            for segment in segments:
+                fpath = "{}/{}".format(dpath, segment)
+                event_time = 0
+                for record in tf.python_io.tf_record_iterator(fpath):
+                    tf_item = TfExampleItem(record)
+                    self.assertTrue(tf_item.event_time >= event_time, "{}, {}".format(tf_item.event_time, event_time))
+                    event_time = tf_item.event_time  ## assert order
+                    self.assertEqual(partition_id, CityHash32(tf_item.raw_id) \
+                        % map_task.output_partition_num)
+                    total_cnt += 1
+        self.assertEqual(total_cnt, self._partition_item_num * self._input_partition_num)
+
+    def _check_merge(self, reduce_task):
+        dpath = os.path.join(self._merge_output_dir, \
+            common.partition_repr(reduce_task.partition_id))
+        fpaths = gfile.ListDirectory(dpath)
+        fpaths = sorted(fpaths, key = lambda fpath: fpath, reverse = False)
+        event_time = 0
+        total_cnt = 0
+        for fpath in fpaths:
+            fpath = os.path.join(dpath, fpath)
+            logging.info("check merge path:{}".format(fpath))
+            for record in tf.python_io.tf_record_iterator(fpath):
+                tf_item = TfExampleItem(record)
+                self.assertTrue(tf_item.event_time >= event_time)
+                event_time = tf_item.event_time
+                total_cnt += 1
+        return total_cnt
+
+
+    def test_portal_worker(self):
+        self._prepare_test()
+        map_task = dp_pb.MapTask()
+        map_task.output_base_dir = self._partition_output_dir
+        map_task.output_partition_num = self._output_partition_num
+        map_task.partition_id = 0
+        for partition_id in range(self._input_partition_num):
+            map_task.fpaths.append(self._get_input_fpath(partition_id))
+
+        # partitioner
+        task = dp_pb.NewTaskResponse()
+        task.map_task.CopyFrom(map_task)
+        self._portal_worker._run_map_task(task.map_task)
+
+        self._check_partitioner(task.map_task)
+
+        # merge
+        total_cnt = 0
+        for partition_id in range(self._output_partition_num):
+            reduce_task = dp_pb.ReduceTask()
+            reduce_task.map_base_dir = self._partition_output_dir
+            reduce_task.reduce_base_dir = self._merge_output_dir
+            reduce_task.partition_id = partition_id
+            self._portal_worker._run_reduce_task(reduce_task)
+            total_cnt += self._check_merge(reduce_task)
+
+        self.assertEqual(total_cnt, self._partition_item_num * self._input_partition_num)
+        self._clean_up()
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    logging.basicConfig(format='%(asctime)s %(message)s')
+    unittest.main()
+
+
