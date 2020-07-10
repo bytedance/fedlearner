614f6b26d9b5a51ef5e86b7dfd6415dfd3db59e8
rapmetal <yijia.lilong@gmail.com>
2020-07-10 18:31:13 +0800

    fix pylint

--- fedlearner/common/metrics.py
+++ fedlearner/common/metrics.py
@@ -75,7 +75,8 @@ class loggingHandler(Handler):
 
 class elasticSearchHandler(Handler):
     def __init__(self, ip, port):
-        from elasticsearch import Elasticsearch
+        from elasticsearch import Elasticsearch # pylint: disable=C0415
+        super(elasticSearchHandler, self).__init__('elasticsearch') 
         self._es = Elasticsearch([ip], port=port)
         # initialize index for elastic search
         if self._es.indices.exists(index='metrics') is not True:
