2080d32c723b19ed7ef0fb0928ae9210f5850227
codemonkey-ll <65523085+codemonkey-ll@users.noreply.github.com>
2020-07-12 18:04:03 +0800

    support csv writer format in data portal (#106)
    * support csv write format in data portal
    
    * add deploy script with csv format
    
    * use output builder
    
    * csv output data file type
    
    * output data file type

--- fedlearner/data_join/cmd/data_portal_worker_cli.py
+++ fedlearner/data_join/cmd/data_portal_worker_cli.py
@@ -47,6 +47,9 @@ if __name__ == '__main__':
     parser.add_argument("--compressed_type", type=str, default='',
                         choices=['', 'ZLIB', 'GZIP'],
                         help='the compressed type of input data file')
+    parser.add_argument("--output_data_file_type", type=str,
+                        default="TF_RECORD",
+                        help="the type for output data")
     parser.add_argument("--batch_size", type=int, default=1024,
                         help="the batch size for raw data reader")
     parser.add_argument("--max_flying_item", type=int, default=300000,
@@ -62,7 +65,8 @@ if __name__ == '__main__':
         partitioner_name="dp_worker_partitioner_{}".format(args.rank_id),
         raw_data_options=raw_data_options,
         batch_processor_options=batch_processor_options,
-        output_item_threshold=args.write_buffer_size)
+        output_item_threshold=args.write_buffer_size,
+        output_builder=args.output_data_file_type)
     merge_options = dp_pb.MergeOptions(
         merger_name="dp_worker_merger_{}".format(args.rank_id),
         raw_data_options=raw_data_options,
