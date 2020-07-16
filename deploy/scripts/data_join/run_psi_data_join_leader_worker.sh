da59565d0cfef0a0ef25323813c025eac4ec17ac
codemonkey-ll <yulele214@qq.com>
2020-07-16 19:33:57 +0800

    remove header

--- deploy/scripts/data_join/run_psi_data_join_leader_worker.sh
+++ deploy/scripts/data_join/run_psi_data_join_leader_worker.sh
@@ -14,26 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/bash
-
-# Copyright 2020 The FedLearner Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 set -ex
 
 WORKER_REPLICAS=`python -c 'import json, os; print(len(json.loads(os.environ["CLUSTER_SPEC"])["clusterSpec"]["Worker"]))'`
-
 echo "${WORKER_REPLICAS:?Need to set WORKER_REPLICAS non-empty}"
 echo "${INDEX:?Need to set INDEX non-empty}"
 if [ "$((WORKER_REPLICAS % 2))" -ne "0" ]; then
