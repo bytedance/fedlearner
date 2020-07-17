5c56ba18dc83a4a88767f61817b61e421ecda69d
codemonkey-ll <yulele214@qq.com>
2020-07-17 07:52:44 +0000

    lint

--- example/data_portal/run.sh
+++ example/data_portal/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+python make_data.py
+
 INPUT_DIR=./data_portal_input_dir
 OUTPUT_DIR=./data_portal_output_dir
 
@@ -9,11 +11,14 @@ python -m fedlearner.data_join.cmd.data_portal_master_service \
     --etcd_base_dir=dp_master \
     --listen_port=50051 \
     --data_portal_name=test_data_portal \
+    --data_portal_type=Streaming \
     --output_partition_num=2 \
     --input_base_dir=${INPUT_DIR} \
     --output_base_dir=${OUTPUT_DIR} \
     --raw_data_publish_dir=raw_data_publish_dir \
-    --use_mock_etcd > master.log 2>&1 &
+    --use_mock_etcd > master.streaming.log 2>&1 &
+
+master_pid=`echo $!`
 
 python -m fedlearner.data_join.cmd.data_portal_worker_cli \
     --master_addr=localhost:50051 \
@@ -21,5 +26,42 @@ python -m fedlearner.data_join.cmd.data_portal_worker_cli \
     --etcd_name=test_data_portal_worker \
     --etcd_addrs=localhost:2379 \
     --etcd_base_dir=dp_worker_0 \
-    --use_mock_etcd > worker.0.log 2>&1 &
+    --use_mock_etcd > worker.streaming.log 2>&1 &
+
+worker_pid=`echo $!`
+wait ${worker_pid} 
+kill ${master_pid}
+rm -rf $OUTPUT_DIR
+
+
+python -m fedlearner.data_join.cmd.data_portal_master_service \
+    --etcd_name=test_data_portal_master \
+    --etcd_addrs=localhost:2379 \
+    --etcd_base_dir=dp_master \
+    --listen_port=50051 \
+    --data_portal_name=test_data_portal \
+    --data_portal_type=PSI \
+    --output_partition_num=2 \
+    --input_base_dir=${INPUT_DIR} \
+    --output_base_dir=${OUTPUT_DIR} \
+    --raw_data_publish_dir=raw_data_publish_dir \
+    --use_mock_etcd > master.PSI.log 2>&1 &
+
+master_pid=`echo $!`
+
+python -m fedlearner.data_join.cmd.data_portal_worker_cli \
+    --master_addr=localhost:50051 \
+    --rank_id=0 \
+    --etcd_name=test_data_portal_worker \
+    --etcd_addrs=localhost:2379 \
+    --etcd_base_dir=dp_worker_0 \
+    --use_mock_etcd > worker.PSI.log 2>&1 &
+
+worker_pid=`echo $!`
+wait ${worker_pid}
+kill ${master_pid}
+rm -rf $OUTPUT_DIR $INPUT_DIR
+
+
+
     
--- fedlearner/data_join/data_portal_job_manager.py
+++ fedlearner/data_join/data_portal_job_manager.py
@@ -27,7 +27,7 @@ from fedlearner.common import data_portal_service_pb2 as dp_pb
 from fedlearner.data_join import common
 from fedlearner.data_join.raw_data_publisher import RawDataPublisher
 from fedlearner.data_join.sort_run_merger import MergedSortRunMeta
-from fedlearner.data_join.sort_run_dumper import SortRunMeta
+from fedlearner.data_join.raw_data_partitioner import RawDataPartitioner
 
 class DataPortalJobManager(object):
     def __init__(self, etcd, portal_name, long_running):
@@ -368,10 +368,9 @@ class DataPortalJobManager(object):
         self._publisher.publish_raw_data(partition_id, fpaths)
 
     def _publish_psi_raw_data(self, partition_id, dpath, fnames):
-        metas = [SortRunMeta.decode_sort_run_meta_from_fname(fname)
+        metas = [RawDataPartitioner.FileMeta.decode_meta_from_fname(fname)
                  for fname in fnames]
-        metas.sort()
-        fpaths = [path.join(dpath, meta.encode_sort_run_fname())
+        fpaths = [path.join(dpath, meta.encode_meta_to_fname())
                   for meta in metas]
         self._publisher.publish_raw_data(partition_id, fpaths)
         self._publisher.finish_raw_data(partition_id)
