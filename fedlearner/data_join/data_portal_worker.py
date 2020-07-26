d66a527b884b7ebc328a58386c0f283b7d62add7
fangchenliaohui <fangchenliaohui@bytedance.com>
2020-07-26 13:41:11 +0800

    rm the useless write_buffer_size for data portal worker

--- fedlearner/data_join/data_portal_worker.py
+++ fedlearner/data_join/data_portal_worker.py
@@ -150,8 +150,7 @@ class DataPortalWorker(object):
             writer_options=self._options.writer_options,
             output_file_dir=task.reduce_base_dir,
             partition_id=task.partition_id,
-            merge_buffer_size=self._options.merge_buffer_size,
-            write_buffer_size=self._options.write_buffer_size
+            merge_buffer_size=self._options.merge_buffer_size
         )
 
     def _run_map_task(self, task):
