ada39e8939bdf763f340cf8f79f6a613150c39d5
Allen Shen <932142511@qq.com>
2020-08-17 11:41:06 +0800

    feat(web_console): form json (#239)
    * feat(web_console): form json
    
    * chore(web_console): make form fields immutable
    * mask sensitive data
    
    * fix: lock version
    
    Co-authored-by: shenshihao <shenshihao.20@bytedance.com>

--- web_console/pages/admin/federation.jsx
+++ web_console/pages/admin/federation.jsx
@@ -9,6 +9,35 @@ import Layout from '../../components/Layout';
 import Form from '../../components/Form';
 import { fetcher } from '../../libs/http';
 import { createFederation, updateFederation } from '../../services';
+import produce from 'immer'
+
+import { K8S_SETTINGS } from '../../constants/form-default'
+
+const fillJSON = (container, path, value) => {
+  if (typeof path === 'string') {
+    path = path.split('.')
+  }
+  if (path.length === 1) {
+    container[path[0]]= value
+    return
+  }
+  if (container[path[0]] === undefined) {
+    container[path[0]] = {}
+  }
+  let c = container[path[0]]
+  fillJSON(c, path.slice(1), value)
+}
+
+const getValueFromJson = (data, path) => {
+  if (typeof path === 'string') {
+    path = path.split('.')
+  }
+  if (path.length === 1) {
+    return data[path[0]]
+  }
+  if (data[path[0]] === undefined) return undefined
+  return getValueFromJson(data[path[0]], path.slice(1))
+}
 
 function useFederationItemStyles() {
   return css`
@@ -69,39 +98,179 @@ function FederationItem({ data, onEdit }) {
   );
 }
 
+const switchK8sSettingsFormType = (data, currType, targetType) => {
+  if (targetType === 'json') {
+    const k8s_settings = {}
+    for (let field of K8S_SETTINGS_FIELDS) {
+      let fieldValue = data[field.key]
+      if (['json', 'name-value'].some(el => el === field.type)) {
+        try {
+          fieldValue = JSON.parse(
+            fieldValue || (field.type === 'name-value' ? '[]' : '{}')
+          )
+        } catch {
+          return {error: `Error occurred when parsing json. Please check ${field.key}`}
+        }
+      }
+      fillJSON(k8s_settings, field.path || [field.key], fieldValue)
+    }
+    // x-federation
+    data['x-federation']
+      && fillJSON(k8s_settings, 'grpc_spec.extraHeaders.x-federation', data['x-federation'])
+
+    return { k8s_data: JSON.stringify(k8s_settings, null, 2) }
+  }
+  if (targetType === 'form') {
+    const formData = {}
+    let value
+    try {
+      value = JSON.parse(data['k8s_data'])
+    } catch (e) {
+      return {error: 'Error occurred when parsing json. Please check.'}
+    }
+
+    for (let field of K8S_SETTINGS_FIELDS) {
+      let fieldValue = getValueFromJson(value, field.path || [field.key])
+      if (['json', 'name-value'].some(el => el === field.type)) {
+        fieldValue = JSON.stringify(fieldValue, null, 2)
+      }
+      formData[field.key] = fieldValue
+    }
+    formData['x-federation'] = getValueFromJson(value, 'grpc_spec.extraHeaders.x-federation')
+
+    return formData
+  }
+}
+
+const K8S_SETTINGS_FIELDS = [
+  { key: 'storage_root_path', default: K8S_SETTINGS.storage_root_path },
+  {
+    key: 'imagePullSecrets',
+    default: 'regcred',
+    span: 12,
+    path: 'global_replica_spec.template.spec.imagePullSecrets'
+  },
+  {
+    key: 'volumes',
+    type: 'json',
+    span: 24,
+    default: K8S_SETTINGS.volumes,
+    props: {
+      minHeight: '150px',
+    },
+    path: 'global_replica_spec.template.spec.volumes' // path为拼接json时的定位
+  },
+  {
+    key: 'env',
+    type: 'name-value',
+    span: 24,
+    default: K8S_SETTINGS.env,
+    props: {
+      minHeight: '150px',
+    },
+    path: 'global_replica_spec.template.spec.containers.envs'
+  },
+  {
+    key: 'volumeMounts',
+    type: 'json',
+    span: 24,
+    default: K8S_SETTINGS.volumeMounts,
+    props: {
+      minHeight: '150px',
+    },
+    path: 'global_replica_spec.template.spec.containers.volumeMounts'
+  },
+  {
+    key: 'grpc_spec',
+    type: 'json',
+    span: 24,
+    default: K8S_SETTINGS.grpc_spec,
+    props: {
+      minHeight: '150px',
+    },
+  },
+  {
+    key: 'leader_peer_spec',
+    type: 'json',
+    span: 24,
+    default: K8S_SETTINGS.leader_peer_spec,
+    props: {
+      minHeight: '150px',
+    },
+  },
+  {
+    key: 'follower_peer_spec',
+    type: 'json',
+    span: 24,
+    default: K8S_SETTINGS.follower_peer_spec,
+    props: {
+      minHeight: '150px',
+    },
+  },
+]
+
 const DEFAULT_FIELDS = [
   { key: 'name', required: true },
-  { key: 'trademark', span: 12 },
+  { key: 'trademark' },
+  { key: 'x-federation' },
   { key: 'email' },
   { key: 'tel', label: 'telephone' },
   { key: 'avatar' },
   {
-    key: 'k8s_settings',
-    type: 'json',
-    required: true,
-    span: 24,
-    props: {
-      minHeight: '300px',
+    groupName: 'k8s_settings',
+    formTypes: ['form', 'json'],
+    onFormTypeChange: switchK8sSettingsFormType,
+    fields: {
+      form: K8S_SETTINGS_FIELDS,
+      json: [
+        {
+          key: 'k8s_data',
+          type: 'json',
+          hideLabel: true,
+          span: 24,
+          props: {
+            minHeight: '500px'
+          }
+        }
+      ]
     },
   },
 ];
 
+/**
+ * set init value and props of fields
+ */
+function fillField(data, field) {
+  let v = getValueFromJson(data, field.path || field.key)
+  if (typeof v === 'object') {
+    v = JSON.stringify(v, null, 2)
+  }
+  field.value = v
+  field.editing = true
+
+  if (field.key === 'name') {
+    field.props = {
+      disabled: true,
+    };
+  }
+  if (field.key === 'x-federation') {
+    let xFederation = getValueFromJson(data, 'k8s_settings.grpc_spec.extraHeaders.x-federation')
+    xFederation && (field.value = xFederation)
+  }
+
+  return field
+}
+
 function mapValueToFields(federation, fields) {
-  return fields.map((x) => {
-    const field = { ...x, value: federation[x.key] };
-
-    if (x.key === 'name') {
-      field.props = {
-        disabled: true,
-      };
-    }
-
-    if (x.key === 'k8s_settings') {
-      field.value = JSON.stringify(field.value, null, 2);
-    }
-
-    return field;
-  });
+  return produce(fields, draft => {
+    draft.map((x) => {
+      if (x.groupName === 'k8s_settings') {
+        x.fields['form'].map(item => fillField(federation['k8s_settings'], item))
+      } else {
+        fillField(federation, x)
+      }
+    })
+  })
 }
 
 export default function FederationList() {
@@ -127,10 +296,46 @@ export default function FederationList() {
     setFields(mapValueToFields(federation, fields));
     setFormVisible(true);
   };
+
+  const checkValue = (field, value) => {
+    if (field.key === 'env' && value) {
+      for (let {name, value} of value) {
+        if (name === '' || value === '') {
+          return 'Please fill env'
+        }
+      }
+    }
+    if (field.type === 'json') {
+      try {
+        JSON.parse(value)
+      } catch {
+        return 'json parse failed.Please check ' + field.key
+      }
+    }
+  }
+
   const handleSubmit = (value) => {
+    const k8s_settings = {}
+    // gen json
+    for (let field of K8S_SETTINGS_FIELDS) {
+      let fieldValue = value.k8s_settings[field.key]
+
+      let error = checkValue(field, fieldValue)
+      if (error) {
+        return {error}
+      }
+      if (field.type === 'json') {
+        fieldValue = JSON.parse(fieldValue)
+      }
+      fieldValue !== undefined && fillJSON(k8s_settings, field.path || [field.key], fieldValue)
+    }
+    // x-federation
+    value['x-federation']
+      && fillJSON(k8s_settings, 'grpc_spec.extraHeaders.x-federation', value['x-federation'])
+
     const json = {
       ...value,
-      k8s_settings: JSON.parse(value.k8s_settings),
+      k8s_settings,
     };
 
     if (currentFederation) {
