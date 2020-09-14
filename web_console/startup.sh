87fa08b89509a8359d3ef04a175d02f6f5eb1449
Junyuan Xie <eric.jy.xie@gmail.com>
2020-09-14 11:31:56 +0800

    fix

--- web_console/startup.sh
+++ web_console/startup.sh
@@ -5,6 +5,6 @@ set -ex
 npx sequelize-cli db:migrate \
     --url="${DB_DIALECT:-mysql}://${DB_USERNAME:-fedlearner}:${DB_PASSWORD:-fedlearner}@${DB_HOST:-127.0.0.1}:${DB_PORT:-3306}/${DB_DATABASE:-fedlearner}"
 
-node bootstrap.js
+node bootstrap.js &
 
-while true; do sleep 1m; done
+while true; do sleep 1000; done
