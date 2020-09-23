d3a96160142eea41fe385db78b8b2e6bf14e8362
Allen Shen <932142511@qq.com>
2020-09-23 11:14:48 +0800

    feat: config replica type (#339)
    * feat(web_console): form json
    
    * chore(web_console): make form fields immutable
    * mask sensitive data
    
    * fix: lock version
    
    * feat(web_console): support popover on nav tab
    
    * feat(WIP): datasource
    
    * feat(web_console): use form meta to implement form convert
    
    * chore(web_console): update doc
    
    * feat(web_console): raw data form:
    * form utils unit test
    * fix logic of `fillJSON`
    
    * fix: raw data submit:
    * fix federation value validator
    
    * fix: image should be written into context
    
    * fix: async response on submit
    
    * fix: ticket not clone job
    
    * chore: use `useMemo` with form fields
    
    * feat(web_console): rewrite `fiilJSON`:
    * support two array mode
    
    * feat(WIP): datasource job form
    * form item `name-value` support ignore keys
    * incremental update form value in group type change
    * TODO: form submit
    
    * feat(web_console): boolean select in form
    
    * feat(WIP): datasource ticket
    * fix header active tab
    
    * feat(web_console): datasource and trainning
    
    * fix(web_console): ticket
    * update form
    * create ticket
    * job link
    * add `filterArrayValue`
    
    * fix(web_console): clone job
    
    * feat(web_console): stop and start job
    
    * fix: update job status
    
    * feat: job federation id filter
    
    * fix: job status
    
    * fix: package-lock
    
    * fix: prerender error
    
    * fix: prerender error
    
    * fix: boolean select;
    * update router of charts
    
    * fix: bool-select
    
    * feat: header dropdown icon
    
    * feat: federation heartbeat
    
    * fix: clone job
    
    * fix: heartbeat
    
    * fix: server ticket select
    
    * fix: raw data in clone
    
    * fix: delete job rpc
    
    * fix: clear formMeta on cancel
    
    * chore: remove raw_data in job form
    
    * feat: show total info of server ticket:
    * fix create jon derect in the index
    * fix server ticket pull in cloning
    
    * fix: useEffect
    
    * fix: server ticket info
    
    * chore: remove mock data
    
    * chore: fix typo
    
    * feat: disable federation with filter
    
    * fix: raw data fill:
    * fix raw data appState
    
    * fix: num_partitions
    
    * fix: raw_data fill:
    * TODO: figure out why not working in some conditions
    
    * feat: use full params to set default:
    * change default value when  modify job type
    
    * feat: select datasource in training job
    
    * feat: config replica type:
    * feat update form value when update
    
    * fix: hide env value in training
    
    * Update form-default.js
    
    Co-authored-by: shenshihao <shenshihao.20@bytedance.com>
    Co-authored-by: Eric Junyuan Xie <piiswrong@users.noreply.github.com>

---
+++ web_console/components/DataSourceSelect.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Select, Popover } from '@zeit-ui/react';
+import useSWR from 'swr';
+import { fetcher } from '../libs/http';
+import { JOB_TYPE_CLASS } from '../constants/job'
+
+export default function ServerTicketSelect({type, ...props}) {
+
+  const filter = el => JOB_TYPE_CLASS.datasource.some(t => el.localdata?.job_type === t)
+
+  const { data } = useSWR(`jobs`, fetcher);
+  const jobs = data?.data?.filter(filter) || []
+
+  const actualValue = jobs.find((x) => x.name === props.localdata?.name)?.name;
+  const actualOnChange = (value) => {
+    props.onChange(value);
+  };
+
+  return (
+    <Select {...props} initialValue={actualValue} value={actualValue} onChange={actualOnChange}>
+      {
+        jobs.map((x) =>
+          <Select.Option key={x.localdata.name} value={x.localdata.name}>
+              {x.localdata.name}
+          </Select.Option>
+        )
+      }
+    </Select>
+  );
+}
