c90dbb279fadd0b12008ba838c8ef2b925d7ac00
shenshihao <shenshihao.20@bytedance.com>
2020-09-24 14:35:27 +0800

    fix: data source and worker nums fill

--- web_console/components/CommonJobList.jsx
+++ web_console/components/CommonJobList.jsx
@@ -128,8 +128,14 @@ function fillField(data, field) {
     }
   }
   else if (field.key === 'datasource') {
-    v = v = getValueFromEnv(data['public_params'], envPath, 'DATA_SOURCE')
-      || getValueFromEnv(data['private_params'], envPath, 'DATA_SOURCE')
+    v = getValueFromEnv(data['client_params'], envPath, 'DATA_SOURCE')
+      || getValueFromEnv(data['server_params'], envPath, 'DATA_SOURCE')
+  }
+  else if (/[/s/S]* num$/.test(field.key)) {
+    let replicaType = field.key.split(' ')[0]
+    let path = `spec.flReplicaSpecs.${replicaType}.replicas`
+    v = getValueFromJson(data['client_params'], path)
+      || getValueFromJson(data['server_params'], path)
   }
 
   if (typeof v === 'object') {
