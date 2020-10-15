e352879c98e9627fca8472aae70eddbd9f62a557
Junyuan Xie <eric.jy.xie@gmail.com>
2020-10-15 17:01:15 +0800

    Show data join metrics

--- web_console/utils/metrics_list.js
+++ web_console/utils/metrics_list.js
@@ -1,9 +1,157 @@
 const metrics_list = {
     data_join: [
-
+        {
+            query: "name%20:%22data_block_dump_duration%22",
+            mode: "avg",
+            title: "data_block_dump_duration"
+        },
+        {
+            query: "name%20:%22data_block_index%22",
+            mode: "avg",
+            title: "data_block_index"
+        },
+        {
+            query: "name%20:%22stats_cum_join_num%22",
+            mode: "avg",
+            title: "stats_cum_join_num"
+        },
+        {
+            query: "name%20:%22actual_cum_join_num%22",
+            mode: "avg",
+            title: "actual_cum_join_num"
+        },
+        {
+            query: "name%20:%22leader_stats_index%22",
+            mode: "avg",
+            title: "leader_stats_index"
+        },
+        {
+            query: "name%20:%22follower_stats_index%22",
+            mode: "avg",
+            title: "follower_stats_index"
+        },
+        {
+            query: "name%20:%22example_id_dump_duration%22",
+            mode: "avg",
+            title: "example_id_dump_duration"
+        },
+        {
+            query: "name%20:%22example_dump_file_index%22",
+            mode: "avg",
+            title: "example_dump_file_index"
+        },
+        {
+            query: "name%20:%22example_id_dumped_index%22",
+            mode: "avg",
+            title: "example_id_dumped_index"
+        },
+        {
+            query: "name%20:%22stats_cum_join_num%22",
+            mode: "avg",
+            title: "stats_cum_join_num"
+        },
+        {
+            query: "name%20:%22actual_cum_join_num%22",
+            mode: "avg",
+            title: "actual_cum_join_num"
+        },
+        {
+            query: "name%20:%22leader_stats_index%22",
+            mode: "avg",
+            title: "leader_stats_index"
+        },
+        {
+            query: "name%20:%22follower_stats_index%22",
+            mode: "avg",
+            title: "follower_stats_index"
+        },
+        {
+            query: "name%20:%22leader_join_rate_percent%22",
+            mode: "avg",
+            title: "leader_join_rate_percent"
+        },
+        {
+            query: "name%20:%22follower_join_rate_percent%22",
+            mode: "avg",
+            title: "follower_join_rate_percent"
+        }
     ],
     psi_data_join: [
-
+        {
+            query: "name%20:%22data_block_dump_duration%22",
+            mode: "avg",
+            title: "data_block_dump_duration"
+        },
+        {
+            query: "name%20:%22data_block_index%22",
+            mode: "avg",
+            title: "data_block_index"
+        },
+        {
+            query: "name%20:%22stats_cum_join_num%22",
+            mode: "avg",
+            title: "stats_cum_join_num"
+        },
+        {
+            query: "name%20:%22actual_cum_join_num%22",
+            mode: "avg",
+            title: "actual_cum_join_num"
+        },
+        {
+            query: "name%20:%22leader_stats_index%22",
+            mode: "avg",
+            title: "leader_stats_index"
+        },
+        {
+            query: "name%20:%22follower_stats_index%22",
+            mode: "avg",
+            title: "follower_stats_index"
+        },
+        {
+            query: "name%20:%22example_id_dump_duration%22",
+            mode: "avg",
+            title: "example_id_dump_duration"
+        },
+        {
+            query: "name%20:%22example_dump_file_index%22",
+            mode: "avg",
+            title: "example_dump_file_index"
+        },
+        {
+            query: "name%20:%22example_id_dumped_index%22",
+            mode: "avg",
+            title: "example_id_dumped_index"
+        },
+        {
+            query: "name%20:%22stats_cum_join_num%22",
+            mode: "avg",
+            title: "stats_cum_join_num"
+        },
+        {
+            query: "name%20:%22actual_cum_join_num%22",
+            mode: "avg",
+            title: "actual_cum_join_num"
+        },
+        {
+            query: "name%20:%22leader_stats_index%22",
+            mode: "avg",
+            title: "leader_stats_index"
+        },
+        {
+            query: "name%20:%22follower_stats_index%22",
+            mode: "avg",
+            title: "follower_stats_index"
+        },
+        {
+            query: "name%20:%22leader_join_rate_percent%22",
+            mode: "avg",
+            title: "leader_join_rate_percent"
+        },
+        {
+            query: "name%20:%22follower_join_rate_percent%22",
+            mode: "avg",
+            title: "follower_join_rate_percent"
+        }
     ],
     tree_model: [
 
