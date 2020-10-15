9cd6d4abe46a55137d233daa2ce366fb9eeca37d
Junyuan Xie <eric.jy.xie@gmail.com>
2020-10-15 16:19:45 +0800

    Add new psi data join entry script

---
+++ deploy/scripts/rsa_psi/run_psi_data_join_master.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Copyright 2020 The FedLearner Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+data_join_master_cmd=/app/deploy/scripts/data_join/run_data_join_master.sh
+
+export RAW_DATA_SUB_DIR="portal_publish_dir/${APPLICATION_ID}_psi_preprocess"
+${data_join_master_cmd}
