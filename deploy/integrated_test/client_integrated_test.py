b966f9f89f8c04784585ced348850576ea1722ce
ADeltanp <45765741+ZhZhang711@users.noreply.github.com>
2020-10-26 19:55:45 +0800

    Full integrated test (#385)
    * add csv data
    
    * fix file_type for tree model
    
    * add field 'label' to tfrecord_raw_data
    
    * fix num_parallel
    
    * add num_parallel for compatibility.
    
    * Revert and add new fake feature in tfrecord raw data for tree model.
    This reverts commit 8ca6061404d4ee5a08edcf0a1a412632b0e18605.
    
    * modified tree trainer for debug purpose, will be reverted
    
    * Revert debug, add send metrics in tree trainer, shrink raw data, full integrated test supported
    
    This reverts commit 977cd45da7f3f4c9ddbc8527350d256769a71f8c.
    
    * add loss_type for run_tree_worker.sh
    
    * eliminated data path
    
    * add num_parallel for compatibility.
    
    * Revert and add new fake feature in tfrecord raw data for tree model.
    This reverts commit 8ca6061404d4ee5a08edcf0a1a412632b0e18605.
    
    * modified tree trainer for debug purpose, will be reverted
    
    * Revert debug, add send metrics in tree trainer, shrink raw data, full integrated test supported
    
    This reverts commit 977cd45da7f3f4c9ddbc8527350d256769a71f8c.
    
    * rebased and modified defaults
    
    * deleted csv data
    
    Co-authored-by: zhangzihui <zhangzihui@bytedance.com>

--- deploy/integrated_test/client_integrated_test.py
+++ deploy/integrated_test/client_integrated_test.py
@@ -2,7 +2,8 @@
 import argparse
 import json
 import requests
-from tools import login, request_and_response, build_raw_data, build_data_join_ticket, build_train_ticket
+from tools import login, request_and_response, build_raw_data, \
+    build_data_join_ticket, build_nn_ticket, build_tree_ticket
 
 
 def build_federation_json(args):
@@ -18,17 +19,61 @@ def build_federation_json(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', type=str)
-    parser.add_argument('--x-federation', type=str)
-    parser.add_argument('--image', type=str)
-    parser.add_argument('--url', type=str)
-    parser.add_argument('--username', type=str)
-    parser.add_argument('--password', type=str)
-    parser.add_argument('--api-version')
+    parser.add_argument('--name',
+                        type=str,
+                        help='Name for peer federation.')
+    parser.add_argument('--x-federation',
+                        type=str,
+                        help='Name for local federation.')
+    parser.add_argument('--image',
+                        type=str,
+                        help='Image address.')
+    parser.add_argument('--data-portal-type',
+                        type=str,
+                        help='Type of raw data, Streaming(default) or PSI.',
+                        choices=['Streaming', 'PSI'],
+                        default='Streaming')
+    parser.add_argument('--model-type',
+                        type=str,
+                        help='Type of train model, (nn model) or tree model.',
+                        choices=['nn_model', 'tree_model'],
+                        default='nn_model')
+    parser.add_argument('--rsa-key-path',
+                        type=str,
+                        help='Path to RSA public key.')
+    parser.add_argument('--rsa-key-pem',
+                        type=str,
+                        help='Either rsa key path or rsa key pem must be given.')
+    parser.add_argument('--url',
+                        type=str,
+                        help='URL to webconsole.',
+                        default='127.0.0.1:1989')
+    parser.add_argument('--username',
+                        type=str,
+                        help='Username of webconsole.',
+                        default='ada')
+    parser.add_argument('--password',
+                        type=str,
+                        help='Password of webconsole.',
+                        default='ada')
+    parser.add_argument('--api-version',
+                        help='API version of webconsole.',
+                        default=1)
     args = parser.parse_args()
+    args.streaming = args.data_portal_type == 'Streaming'
+    if not args.streaming:
+        args.cmd_args = {'Master': ["/app/deploy/scripts/rsa_psi/run_psi_data_join_master.sh"],
+                         'Worker': ["/app/deploy/scripts/rsa_psi/run_psi_data_join_worker.sh"]}
+        if args.rsa_key_pem is not None:
+            args.psi_extras = [{"name": "RSA_KEY_PEM", "value": args.rsa_key_pem}]
+        elif args.rsa_key_path is not None:
+            args.psi_extras = [{"name": "RSA_KEY_PATH", "value": args.rsa_key_path}]
+        else:
+            raise Exception('Either RSA_KEY_PEN or RSA_KEY_PATH must be provided when using PSI.')
+        args.psi_extras.append({"name": "SIGN_RPC_TIMEOUT_MS", "value": "128000"})
+
     args.url = args.url.strip().rstrip('/') + '/api/v' + str(args.api_version)
     cookie = login(args)
-
     federation_json, suffix = build_federation_json(args)
     federation_id, federation_name = request_and_response(args=args,
                                                           url=args.url + '/federations',
@@ -45,15 +90,22 @@ if __name__ == '__main__':
     requests.post(url=args.url + '/raw_data/' + str(raw_data_id) + '/submit', cookies=cookie)
 
     join_ticket_json, suffix = build_data_join_ticket(args, federation_id, raw_data_name,
-                                                      'template_json/template_join_ticket.json', 'Leader')
+                                                      'template_json/template_streaming_join_ticket.json'
+                                                      if args.streaming
+                                                      else 'template_json/template_psi_join_ticket.json',
+                                                      'Leader' if args.streaming else 'Follower')
     join_ticket_id, join_ticket_name = request_and_response(args=args,
                                                             url=args.url + '/tickets',
                                                             json_data=join_ticket_json,
                                                             cookies=cookie,
                                                             name_suffix=suffix)
 
-    train_ticket_json, suffix = build_train_ticket(args, federation_id,
-                                                   'template_json/template_train_ticket.json', 'Follower')
+    if args.model_type == 'nn_model':
+        train_ticket_json, suffix = build_nn_ticket(args, federation_id,
+                                                    'template_json/template_nn_ticket.json', 'Follower')
+    else:
+        train_ticket_json, suffix = build_tree_ticket(args, federation_id,
+                                                      'template_json/template_tree_ticket.json', 'Follower')
     train_ticket_id, train_ticket_name = request_and_response(args=args,
                                                               url=args.url + '/tickets',
                                                               json_data=train_ticket_json,
