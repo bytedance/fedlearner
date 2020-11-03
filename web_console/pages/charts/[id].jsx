86414d7e5348a27459dec3f6c6b797291170c420
Allen Shen <932142511@qq.com>
2020-11-03 16:49:20 +0800

    feat: custom metrics (#399)
    Co-authored-by: shenshihao <shenshihao.20@bytedance.com>

--- web_console/pages/charts/[id].jsx
+++ web_console/pages/charts/[id].jsx
@@ -1,8 +1,8 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useCallback, useRef } from 'react';
 import useSWR from 'swr';
 import { useRouter } from 'next/router';
 import css from 'styled-jsx/css';
-import { Text, Card, Grid } from '@zeit-ui/react';
+import { Text, Card, Grid, useInput, Input, Button } from '@zeit-ui/react';
 import { fetcher } from '../../libs/http';
 import Layout from '../../components/Layout';
 import getJobDashboardUrls from '../../utils/kibana';
@@ -14,6 +14,13 @@ function useStyle() {
       width: 100%;
       height: 80vh;
     }
+    .custom-metrics {
+      display: flex;
+      align-items: center;
+    }
+    .label {
+      padding: 0 12px;
+    }
   `;
 }
 
@@ -23,16 +30,50 @@ export default function Chart(props) {
   const { id } = router.query;
   const { data, isValidating } = useSWR(id ? `job/${id}` : null, fetcher);
   const job = data?.data?.localdata;
+
+  const customerUrls = useRef([])
   const urls = useMemo(() => {
     if (job) return getJobDashboardUrls(job);
     return [];
   }, [job]);
 
+  const { state: customMode, reset: resetCustomMode, bindings: customModeBindings } = useInput('avg')
+  const { state: customTitle, reset: resetCustomTitle, bindings: customTitleBindings } = useInput('')
+  const onAddMetrics = useCallback(() => {
+    if (!job) return
+    let mockjob = {name: 'mock', job_type: 'data_join', created_at: '2020-10-20'}
+    customerUrls.current.push(getJobDashboardUrls(mockjob, {customMode, customTitle}))
+    resetCustomMode()
+    resetCustomTitle()
+  }, [job, customMode, customTitle])
+
   return (
     <Layout theme={props.theme} toggleTheme={props.toggleTheme}>
+      <Card style={{marginBottom: '12px'}}>
+        <div className="custom-metrics">
+          <div className="label">Mode</div>
+          <Input width="200px" {...customModeBindings}/>
+          <div className="label">Title</div>
+          <Input width="200px" {...customTitleBindings}/>
+          <Button style={{marginLeft: '12px'}} onClick={onAddMetrics} auto>Add Metrics</Button>
+        </div>
+      </Card>
       {isValidating && <Text h2>Loading...</Text>}
-      {urls.length > 0
-        ? (
+      {
+        customerUrls.current.length > 0 && (
+          <Grid.Container gap={2}>
+            {customerUrls.current.map((url) => (
+              <Grid sm={24} md={24}>
+                <Card>
+                  <iframe src={url} />
+                </Card>
+              </Grid>
+            ))}
+          </Grid.Container>
+        )
+      }
+      {
+        urls.length > 0 && (
           <Grid.Container gap={2}>
             {urls.map((url) => (
               <Grid sm={24} md={24}>
@@ -43,7 +84,12 @@ export default function Chart(props) {
             ))}
           </Grid.Container>
         )
-        : <Text h2>No metrics yet :(</Text>}
+      }
+      {
+        customerUrls.current.length === 0
+          && urls.length === 0
+          && <Text h2>No metrics yet :(</Text>
+      }
 
       <style jsx>{styles}</style>
     </Layout>
