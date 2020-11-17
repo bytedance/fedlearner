0300eb2c55a78d0637c77f81f5c2156f6a513fef
duanbing <hibduan@gmail.com>
2020-11-17 18:13:21 +0800

    Enable summary dump

--- deploy/scripts/trainer/run_trainer_worker.sh
+++ deploy/scripts/trainer/run_trainer_worker.sh
@@ -60,6 +60,7 @@ save_checkpoint_steps=$(normalize_env_to_args "--save-checkpoint-steps" "$SAVE_C
 save_checkpoint_secs=$(normalize_env_to_args "--save-checkpoint-secs" "$SAVE_CHECKPOINT_SECS")
 sparse_estimator=$(normalize_env_to_args "--sparse-estimator" "$SPARSE_ESTIMATOR")
 summary_save_steps=$(normalize_env_to_args "--summary-save-steps" "$SUMMARY_SAVE_STEPS")
+summary_path=$(normalize_env_to_args "--summary-path" "$SUMMARY_PATH")
 batch_size=$(normalize_env_to_args "--batch-size" "$BATCH_SIZE")
 learning_rate=$(normalize_env_to_args "--learning-rate" "$LEARNING_RATE")
 
@@ -75,4 +76,4 @@ python main.py \
     --export-path="$OUTPUT_BASE_DIR/exported_models" \
     $verbosity \
     $save_checkpoint_steps $sparse_estimator $summary_save_steps \
-    $save_checkpoint_secs $batch_size $learning_rate
+    $save_checkpoint_secs $batch_size $learning_rate $summary_path
