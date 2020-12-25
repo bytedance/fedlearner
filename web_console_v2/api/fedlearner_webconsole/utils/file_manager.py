79b76ce5a15fd2f04b88f83cd8845483d60c1350
Fan Lin <70993766+tjulinfan@users.noreply.github.com>
2020-12-25 21:18:11 +0800

    [WebConsoleV2] Implements file managers to handle files. (#478)

---
+++ web_console_v2/api/fedlearner_webconsole/utils/file_manager.py
@@ -0,0 +1,159 @@
+# Copyright 2020 The FedLearner Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+import importlib
+import logging
+import os
+import shutil
+
+from pathlib import Path
+from typing import List
+
+from snakebite.client import AutoConfigClient
+
+
+class FileManagerBase(object):
+    """A base interface for file manager, please implement this interface
+    if you have specific logic to handle files, for example, HDFS with ACL."""
+    def can_handle(self, path: str) -> bool:
+        """If the manager can handle such file."""
+        raise NotImplementedError()
+
+    def ls(self, path: str, recursive=False) -> List[str]:
+        """Lists files under a path."""
+        raise NotImplementedError()
+
+    def move(self, source: str, destination: str) -> bool:
+        """Moves a file from source to destination, if destination
+        is a folder then move into that folder."""
+        raise NotImplementedError()
+
+    def remove(self, path: str) -> bool:
+        """Removes files under a path."""
+        raise NotImplementedError()
+
+
+class DefaultFileManager(FileManagerBase):
+    """Default file manager for native file system or NFS."""
+
+    def can_handle(self, path):
+        return path.startswith('/')
+
+    def ls(self, path: str, recursive=False) -> List[str]:
+        if not Path(path).exists():
+            return []
+        # If it is a file
+        if Path(path).is_file():
+            return [path]
+
+        files = []
+        if recursive:
+            for root, dirs, fs in os.walk(path):
+                for file in fs:
+                    files.append(os.path.join(root, file))
+        else:
+            files = [
+                os.path.join(path, file)
+                for file in os.listdir(path)
+            ]
+        # Files only
+        return list(filter(lambda f: Path(f).is_file(), files))
+
+    def move(self, source: str, destination: str) -> bool:
+        try:
+            shutil.move(source, destination)
+            return True
+        except Exception as e:  # pylint: disable=broad-except
+            logging.error('Error during move %s', e)
+            return False
+
+    def remove(self, path: str) -> bool:
+        try:
+            if os.path.isfile(path):
+                os.remove(path)
+                return True
+            if os.path.isdir(path):
+                shutil.rmtree(path)
+                return True
+        except Exception as e:  # pylint: disable=broad-except
+            logging.error('Error during remove %s', e)
+        return False
+
+
+class HdfsFileManager(FileManagerBase):
+    """A wrapper of snakebite client."""
+
+    def can_handle(self, path):
+        return path.startswith('hdfs://')
+
+    def __init__(self):
+        self._client = AutoConfigClient()
+
+    def ls(self, path: str, recursive=False) -> List[str]:
+        files = []
+        for file in self._client.ls([path], recurse=recursive):
+            if file['file_type'] == 'f':
+                files.append(file['path'])
+        return files
+
+    def move(self, source: str, destination: str) -> bool:
+        return len(list(self._client.rename([source], destination))) > 0
+
+    def remove(self, path: str) -> bool:
+        return len(list(self._client.delete([path]))) > 0
+
+
+class FileManager(FileManagerBase):
+    """A centralized manager to handle files.
+
+    Please extend `FileManagerBase` and put the class path into
+    `CUSTOMIZED_FILE_MANAGER`. For example,
+    'fedlearner_webconsole.utils.file_manager:HdfsFileManager'"""
+    def __init__(self):
+        self._file_managers = []
+        cfm_path = os.environ.get('CUSTOMIZED_FILE_MANAGER')
+        if cfm_path:
+            module_path, class_name = cfm_path.split(':')
+            module = importlib.import_module(module_path)
+            # Dynamically construct a file manager
+            customized_file_manager = getattr(module, class_name)
+            self._file_managers.append(customized_file_manager())
+        self._file_managers.append(DefaultFileManager())
+
+    def can_handle(self, path):
+        for fm in self._file_managers:
+            if fm.can_handle(path):
+                return True
+        return False
+
+    def ls(self, path: str, recursive=False) -> List[str]:
+        for fm in self._file_managers:
+            if fm.can_handle(path):
+                return fm.ls(path, recursive=recursive)
+        raise RuntimeError('ls is not supported')
+
+    def move(self, source: str, destination: str) -> bool:
+        logging.info('Moving files from [%s] to [%s]', source, destination)
+        for fm in self._file_managers:
+            if fm.can_handle(source) and fm.can_handle(destination):
+                return fm.move(source, destination)
+        raise RuntimeError('move is not supported')
+
+    def remove(self, path: str) -> bool:
+        logging.info('Removing file [%s]', path)
+        for fm in self._file_managers:
+            if fm.can_handle(path):
+                return fm.remove(path)
+        raise RuntimeError('remove is not supported')
---
+++ web_console_v2/api/test/fedlearner_webconsole/utils/file_manager_test.py
@@ -0,0 +1,185 @@
+# Copyright 2020 The FedLearner Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+import os
+import shutil
+import tempfile
+import unittest
+
+from pathlib import Path
+from unittest.mock import patch, MagicMock
+
+from fedlearner_webconsole.utils.file_manager import (
+    DefaultFileManager, HdfsFileManager, FileManager)
+
+
+class DefaultFileManagerTest(unittest.TestCase):
+    def setUp(self):
+        # Create a temporary directory
+        self._test_dir = tempfile.mkdtemp()
+        Path(self._test_dir).joinpath('f1.txt').touch()
+        Path(self._test_dir).joinpath('f2.txt').touch()
+        subdir = Path(self._test_dir).joinpath('subdir')
+        subdir.mkdir()
+        subdir.joinpath('s1.txt').touch()
+
+        self._fm = DefaultFileManager()
+
+    def tearDown(self):
+        # Remove the directory after the test
+        shutil.rmtree(self._test_dir)
+
+    def _get_temp_path(self, file_path: str = None) -> str:
+        return str(Path(self._test_dir, file_path or '').absolute())
+
+    def test_can_handle(self):
+        self.assertTrue(self._fm.can_handle('/data/abc'))
+        self.assertFalse(self._fm.can_handle('data'))
+
+    def test_ls(self):
+        # List file
+        self.assertEqual(self._fm.ls(self._get_temp_path('f1.txt')),
+                         [self._get_temp_path('f1.txt')])
+        # List folder
+        self.assertEqual(
+            sorted(self._fm.ls(self._get_temp_path())),
+            sorted([
+                self._get_temp_path('f1.txt'),
+                self._get_temp_path('f2.txt')
+            ]))
+        # List folder recursively
+        self.assertEqual(
+            sorted(self._fm.ls(self._get_temp_path(), recursive=True)),
+            sorted([
+                self._get_temp_path('f1.txt'),
+                self._get_temp_path('f2.txt'),
+                self._get_temp_path('subdir/s1.txt')
+            ]))
+
+    def test_move(self):
+        # Moves to another folder
+        self._fm.move(self._get_temp_path('f1.txt'), self._get_temp_path('subdir/'))
+        self.assertEqual(
+            sorted(self._fm.ls(self._get_temp_path('subdir'))),
+            sorted([
+                self._get_temp_path('subdir/s1.txt'),
+                self._get_temp_path('subdir/f1.txt')
+            ]))
+        # Renames
+        self._fm.move(self._get_temp_path('f2.txt'), self._get_temp_path('f3.txt'))
+        self.assertEqual(
+            self._fm.ls(self._get_temp_path('f2.txt')), [])
+        self.assertEqual(
+            self._fm.ls(self._get_temp_path('f3.txt')),
+            [self._get_temp_path('f3.txt')])
+
+    def test_remove(self):
+        self._fm.remove(self._get_temp_path('f1.txt'))
+        self._fm.remove(self._get_temp_path('subdir'))
+        self.assertEqual(
+            self._fm.ls(self._get_temp_path(), recursive=True),
+            [self._get_temp_path('f2.txt')])
+
+
+class HdfsFileManagerTest(unittest.TestCase):
+    def setUp(self):
+        self._mock_client = MagicMock()
+        self._client_patcher = patch(
+            'fedlearner_webconsole.utils.file_manager.AutoConfigClient')
+        self._client_patcher.start().return_value = self._mock_client
+        self._fm = HdfsFileManager()
+
+    def tearDown(self):
+        self._client_patcher.stop()
+
+    def test_can_handle(self):
+        self.assertFalse(self._fm.can_handle('/data/abc'))
+        self.assertTrue(self._fm.can_handle('hdfs://abc'))
+
+    def test_ls(self):
+        mock_ls = MagicMock()
+        self._mock_client.ls = mock_ls
+        mock_ls.return_value = [
+            {'file_type': 'f', 'path': '/data/abc'},
+            {'file_type': 'd', 'path': '/data'}
+        ]
+        self.assertEqual(
+            self._fm.ls('/data', recursive=True),
+            ['/data/abc']
+        )
+        mock_ls.assert_called_once_with(['/data'], recurse=True)
+
+    @staticmethod
+    def _yield_files(files):
+        for file in files:
+            yield file
+
+    def test_move(self):
+        mock_rename = MagicMock()
+        self._mock_client.rename = mock_rename
+        mock_rename.return_value = self._yield_files(['/data/123'])
+        self.assertTrue(self._fm.move('/data/abc', '/data/123'))
+        mock_rename.assert_called_once_with(['/data/abc'], '/data/123')
+
+        mock_rename.return_value = self._yield_files([])
+        self.assertFalse(self._fm.move('/data/abc', '/data/123'))
+
+    def test_remove(self):
+        mock_delete = MagicMock()
+        self._mock_client.delete = mock_delete
+        mock_delete.return_value = self._yield_files(['/data/123'])
+        self.assertTrue(self._fm.remove('/data/123'))
+        mock_delete.assert_called_once_with(['/data/123'])
+
+        mock_delete.return_value = self._yield_files([])
+        self.assertFalse(self._fm.remove('/data/123'))
+
+
+class FileManagerTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        os.environ['CUSTOMIZED_FILE_MANAGER'] = 'testing.fake_file_manager:FakeFileManager'
+
+    @classmethod
+    def tearDownClass(cls):
+        del os.environ['CUSTOMIZED_FILE_MANAGER']
+
+    def setUp(self):
+        self._fm = FileManager()
+
+    def test_can_handle(self):
+        self.assertTrue(self._fm.can_handle('fake://123'))
+        # Falls back to default manager
+        self.assertTrue(self._fm.can_handle('/data/123'))
+        self.assertFalse(self._fm.can_handle('hdfs://123'))
+
+    def test_ls(self):
+        self.assertEqual(self._fm.ls('fake://data'), ['fake://data/f1.txt'])
+
+    def test_move(self):
+        self.assertTrue(self._fm.move('fake://move/123', 'fake://move/234'))
+        self.assertFalse(self._fm.move('fake://do_not_move/123', 'fake://move/234'))
+        # No file manager can handle this
+        self.assertRaises(RuntimeError, lambda: self._fm.move('hdfs://123', 'fake://abc'))
+
+    def test_remove(self):
+        self.assertTrue(self._fm.remove('fake://remove/123'))
+        self.assertFalse(self._fm.remove('fake://do_not_remove/123'))
+        # No file manager can handle this
+        self.assertRaises(RuntimeError, lambda: self._fm.remove('hdfs://123'))
+
+
+if __name__ == '__main__':
+    unittest.main()
