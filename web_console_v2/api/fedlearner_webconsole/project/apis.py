eaaf9d201699f01cd830e6696a2f41c90735b35f
Bingchang Chen <19990626.love@163.com>
2021-01-15 20:33:55 +0800

    [WebConsoleV2] Add workflow num in list project (#512)

--- web_console_v2/api/fedlearner_webconsole/project/apis.py
+++ web_console_v2/api/fedlearner_webconsole/project/apis.py
@@ -17,9 +17,12 @@
 
 from enum import Enum
 from uuid import uuid4
+
+from sqlalchemy.sql import func
 from flask import request
 from flask_restful import Resource, Api, reqparse
 from google.protobuf.json_format import ParseDict
+
 from fedlearner_webconsole.db import db
 from fedlearner_webconsole.k8s_client import get_client
 from fedlearner_webconsole.project.models import Project
@@ -30,6 +33,7 @@ from fedlearner_webconsole.project.add_on \
     import parse_certificates, create_add_on
 from fedlearner_webconsole.exceptions \
     import InvalidArgumentException, NotFoundException
+from fedlearner_webconsole.workflow.models import Workflow
 
 _CERTIFICATE_FILE_NAMES = [
     'client/client.pem', 'client/client.key', 'client/intermediate.pem',
@@ -138,7 +142,16 @@ class ProjectsApi(Resource):
         return {'data': new_project.to_dict()}
 
     def get(self):
-        return {'data': [project.to_dict() for project in Project.query.all()]}
+        # TODO: Not count soft-deleted workflow
+        projects = db.session.query(
+            Project, func.count(Workflow.id).label('num_workflow'))\
+            .join(Workflow.project).group_by(Project.id).all()
+        result = []
+        for project in projects:
+            project_dict = project.Project.to_dict()
+            project_dict['num_workflow'] = project.num_workflow
+            result.append(project_dict)
+        return {'data': result}
 
 
 class ProjectApi(Resource):
