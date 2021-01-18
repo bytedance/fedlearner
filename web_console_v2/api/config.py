db99ada9a35dd299bcd388cccc587c5ddf481bf9
Fan Lin (林凡) <linfan.fine@bytedance.com>
2021-01-18 21:52:05 +0800

    [WebConsoleV2] Gets jwt secret key from env.

--- web_console_v2/api/config.py
+++ web_console_v2/api/config.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = secrets.token_urlsafe(64)
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY',
+                               secrets.token_urlsafe(64))
     PROPAGATE_EXCEPTIONS = True
     LOGGING_LEVEL = logging.INFO
     GRPC_LISTEN_PORT = 1990
