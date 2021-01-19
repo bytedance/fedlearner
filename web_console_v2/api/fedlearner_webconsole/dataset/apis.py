f62bb5af660a4b41fa0224b9eedbd6a08eceb05e
Bingchang Chen <19990626.love@163.com>
2021-01-19 12:39:18 +0800

    [WebConsoleV2] Create dataset by importing (#522)

---
+++ web_console_v2/api/fedlearner_webconsole/dataset/apis.py
@@ -0,0 +1,113 @@
+# Copyright 2020 The FedLearner Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+# pylint: disable=raise-missing-from
+
+import datetime
+
+from google.protobuf.json_format import ParseDict
+from flask_restful import Resource, Api, reqparse
+
+from fedlearner_webconsole.dataset.models import (Dataset, DatasetType,
+                                                  BatchState, DataBatch)
+from fedlearner_webconsole.exceptions import (InvalidArgumentException,
+                                              NotFoundException)
+from fedlearner_webconsole.db import db
+from fedlearner_webconsole.proto.dataset_pb2 import DatasetSource
+
+_FORMAT_ERROR_MESSAGE = '{} is empty'
+
+
+class DatasetsApi(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required=True,
+                            type=str, help=_FORMAT_ERROR_MESSAGE.format('name'))
+        parser.add_argument('type', required=True,
+                            type=DatasetType,
+                            help=_FORMAT_ERROR_MESSAGE.format('type'))
+        parser.add_argument('external_storage_path', type=str)
+        parser.add_argument('comment', type=str)
+        body = parser.parse_args()
+        name = body.get('name')
+        dataset_type = body.get('type')
+        external_storage_path = body.get('external_storage_path')
+        comment = body.get('comment')
+
+        if Dataset.query.filter_by(name=name).first() is not None:
+            raise InvalidArgumentException(
+                details='Dataset {} already exists'.format(name))
+        try:
+            # Create dataset
+            dataset = Dataset(
+                name=name,
+                type=dataset_type,
+                external_storage_path=external_storage_path,
+                comment=comment)
+            db.session.add(dataset)
+            # TODO: scan cronjob
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise InvalidArgumentException(details=str(e))
+        return {'data': dataset.to_dict()}
+
+
+class BatchesApi(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('dataset_id', type=int, required=True,
+                            help=_FORMAT_ERROR_MESSAGE.format('dataset_id'))
+        parser.add_argument('event_time', type=int)
+        parser.add_argument('files', required=True, type=str, action='append',
+                            help=_FORMAT_ERROR_MESSAGE.format('files'))
+        parser.add_argument('move', type=bool)
+        parser.add_argument('comment', type=str)
+        body = parser.parse_args()
+        dataset_id = body.get('dataset_id')
+        event_time = body.get('event_time')
+        files = body.get('files')
+        move = body.get('move', False)
+        comment = body.get('comment')
+
+        dataset = Dataset.query.filter_by(id=dataset_id).first()
+        if dataset is None:
+            raise NotFoundException()
+        if dataset.external_storage_path:
+            raise InvalidArgumentException(
+                details='Cannot import into dataset for scanning')
+        if event_time is None and dataset.type == DatasetType.STREAMING:
+            raise InvalidArgumentException(
+                details='data_batch.event_time is empty')
+
+        # Create batch
+        batch = DataBatch()
+        # Use current timestamp to fill when type is PSI
+        batch.event_time = datetime.datetime.fromtimestamp(
+            event_time or datetime.datetime.now().timestamp())
+        batch.dataset_id = dataset.id
+        batch.comment = comment
+        batch.state = BatchState.IMPORTING
+        batch.num_file = len(files)
+        batch.set_source(ParseDict({'files': files}, DatasetSource()))
+        # TODO: Call scheduler to import
+        db.session.add(batch)
+        db.session.commit()
+        return {'data': batch.to_dict()}
+
+
+def initialize_dataset_apis(api: Api):
+    api.add_resource(DatasetsApi, '/datasets')
+    api.add_resource(BatchesApi, '/data_batches')
---
+++ web_console_v2/api/test/fedlearner_webconsole/dataset/apis_test.py
@@ -0,0 +1,84 @@
+# Copyright 2020 The FedLearner Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+
+import json
+import datetime
+import unittest
+from http import HTTPStatus
+
+from testing.common import BaseTestCase
+from fedlearner_webconsole.db import db
+from fedlearner_webconsole.dataset.models import (Dataset, DatasetType,
+                                                  DataBatch)
+
+
+class DatasetApiTest(BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.default_dataset = Dataset()
+        self.default_dataset.name = 'default_dataset'
+        self.default_dataset.type = DatasetType.STREAMING
+        self.default_dataset.comment = 'test comment'
+        db.session.add(self.default_dataset)
+        db.session.commit()
+
+    def test_post_dataset(self):
+        name = 'test_post_dataset'
+        dataset_type = DatasetType.STREAMING.value
+        comment = 'test comment'
+        create_response = self.client.post(
+            '/api/v2/datasets',
+            data=json.dumps({
+                'name': name,
+                'type': dataset_type,
+                'comment': comment
+            }),
+            content_type='application/json')
+        self.assertEqual(create_response.status_code, HTTPStatus.OK)
+        created_dataset = json.loads(create_response.data).get('data')
+
+        queried_dataset = Dataset.query.filter_by(
+            id=created_dataset.get('id')).first()
+        self.assertEqual(created_dataset, queried_dataset.to_dict())
+
+    def test_post_data_batch(self):
+        dataset_id = self.default_dataset.id
+        event_time = int(datetime.datetime.now().timestamp())
+        files = ['/data/upload/1.csv', '/data/upload/2.csv']
+        move = False
+        comment = 'test post comment'
+        create_response = self.client.post(
+            '/api/v2/data_batches',
+            data=json.dumps({
+                'dataset_id': dataset_id,
+                'event_time': event_time,
+                'files': files,
+                'move': move,
+                'comment': comment
+            }),
+            content_type='application/json')
+        self.assertEqual(create_response.status_code, HTTPStatus.OK)
+        created_data_batch = json.loads(create_response.data).get('data')
+
+        queried_data_batch = DataBatch.query.filter_by(
+            event_time=datetime.datetime.fromtimestamp(event_time),
+            dataset_id=dataset_id).first()
+        self.assertEqual(created_data_batch, queried_data_batch.to_dict())
+
+
+if __name__ == '__main__':
+    unittest.main()
