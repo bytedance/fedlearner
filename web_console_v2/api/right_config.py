83a241d8abd891d05c95966dcbbe05c19ec7327d
xiangyuxuan.prs <xiangyuxuan.prs@bytedance.com>
2021-01-20 19:06:38 +0800

    test in local

---
+++ web_console_v2/api/right_config.py
@@ -0,0 +1,31 @@
+# Copyright 2020 The FedLearner Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+
+import os
+import logging
+import secrets
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Right(object):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'right_app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'secret'
+    PROPAGATE_EXCEPTIONS = True
+    LOGGING_LEVEL = logging.DEBUG
+    GRPC_LISTEN_PORT = 1991
+    ES_HOST = 'fedlearner-stack-elasticsearch-client'
+    ES_PORT = 9200
---
+++ web_console_v2/api/right_manage.py
@@ -0,0 +1,71 @@
+# Copyright 2020 The FedLearner Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+import os
+import logging
+import secrets
+from google.protobuf.json_format import ParseDict
+from fedlearner_webconsole.app import create_app
+from fedlearner_webconsole.db import db
+from fedlearner_webconsole.auth.models import User
+from fedlearner_webconsole.proto import project_pb2
+from fedlearner_webconsole.project.models import Project
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+right_app = create_app('right_config.Right')
+# export FLASK_APP=right_manage:right_app
+# flask create-db
+# export FLASK_ENV=development
+# flask run --host=0.0.0.0 -p 8000
+
+
+@right_app.cli.command('create-db')
+def create_db():
+    db.create_all()
+    user = User(username='ada')
+    user.set_password('ada')
+    db.session.add(user)
+    config = {
+        'name': 'test',
+        'domain_name': 'fl-follower.com',
+        'participants': [
+            {
+                'name': 'party_leader',
+                'url': '127.0.0.1:1993',
+                'domain_name': 'fl-leader.com',
+                'grpc_spec': {
+                    'peer_url': '127.0.0.1:1993',
+                }
+            }
+        ],
+        'variables': [
+            {
+                'name': 'namespace',
+                'value': 'follower'
+            },
+            {
+                'name': 'basic_envs',
+                'value': '{}'
+            },
+            {
+                'name': 'storage_root_dir',
+                'value': '/'
+            }
+        ]
+    }
+    project = Project(name='test',
+                      config=ParseDict(config,
+                                       project_pb2.Project()).SerializeToString())
+    db.session.add(project)
+    db.session.commit()
