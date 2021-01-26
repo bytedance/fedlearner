55a961d6105ae93a9398c85b2aaf4c393da9928f
Fan Lin (林凡) <linfan.fine@bytedance.com>
2021-01-26 19:01:39 +0800

    [WebConsoleV2] Makes DB configurable.

--- web_console_v2/api/config.py
+++ web_console_v2/api/config.py
@@ -21,7 +21,10 @@ import secrets
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'SQLALCHEMY_DATABASE_URI',
+        'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY',
                                secrets.token_urlsafe(64))
