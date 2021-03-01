c058a90d100868ab4efe697c75351741cd803032
waynecz <chengzhou.weng@gmail.com>
2021-03-01 11:51:26 +0800

    [WebConsoleV2] Fix timezone when format timestamp

--- web_console_v2/client/src/shared/date.test.ts
+++ web_console_v2/client/src/shared/date.test.ts
@@ -1,14 +1,15 @@
 import { formatTimestamp, fomatTimeCount } from './date';
 
+// Beijing time
 const DATE_20201_01_21_12_58_23 = 1611205103;
 
 describe('Date formatters', () => {
-  // it('formatTimestamp', () => {
-  //   expect(formatTimestamp(DATE_20201_01_21_12_58_23)).toBe('2021-01-21 12:58:23');
-  //   expect(formatTimestamp(DATE_20201_01_21_12_58_23, 'MM/DD/YYYY')).toBe('01/21/2021');
-  //   expect(formatTimestamp(DATE_20201_01_21_12_58_23 * 1000)).toBe('2021-01-21 12:58:23');
-  //   expect(formatTimestamp(DATE_20201_01_21_12_58_23, 'MM/DD/YYYY')).toBe('01/21/2021');
-  // });
+  it('formatTimestamp', () => {
+    expect(formatTimestamp(DATE_20201_01_21_12_58_23)).toBe('2021-01-21 12:58:23');
+    expect(formatTimestamp(DATE_20201_01_21_12_58_23, 'MM/DD/YYYY')).toBe('01/21/2021');
+    expect(formatTimestamp(DATE_20201_01_21_12_58_23 * 1000)).toBe('2021-01-21 12:58:23');
+    expect(formatTimestamp(DATE_20201_01_21_12_58_23, 'MM/DD/YYYY')).toBe('01/21/2021');
+  });
 
   it('fomatTimeCount', () => {
     expect(fomatTimeCount(3600)).toBe('01:00:00');
