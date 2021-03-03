6371ee1cb529833bd00fb3feccb4374175893b6f
Bingchang Chen <19990626.love@163.com>
2021-03-03 22:37:33 +0800

    [WebConsoleV2] Verify certificates (#614)

--- web_console_v2/api/test/fedlearner_webconsole/project/apis_test.py
+++ web_console_v2/api/test/fedlearner_webconsole/project/apis_test.py
@@ -20,11 +20,14 @@ import unittest
 from base64 import b64encode
 from http import HTTPStatus
 from google.protobuf.json_format import ParseDict
+from unittest.mock import patch, MagicMock
+
 from testing.common import BaseTestCase
 from fedlearner_webconsole.db import db
 from fedlearner_webconsole.project.models import Project
-from fedlearner_webconsole.project.add_on import parse_certificates
-from fedlearner_webconsole.proto.project_pb2 import Project as ProjectProto, CertificateStorage
+from fedlearner_webconsole.project.add_on import parse_certificates, verify_certificates
+from fedlearner_webconsole.proto.project_pb2 import Project as ProjectProto, \
+    CertificateStorage
 from fedlearner_webconsole.workflow.models import Workflow
 
 
@@ -55,7 +58,7 @@ class ProjectApiTest(BaseTestCase):
         self.default_project.set_certificate(ParseDict({
             'domain_name_to_cert': {'fl-test.com':
                                         {'certs':
-                                             parse_certificates(self.TEST_CERTIFICATES)}},
+                                             parse_certificates(self.TEST_CERTIFICATES)}}
         }, CertificateStorage()))
         self.default_project.comment = 'test comment'
         db.session.add(self.default_project)
@@ -78,7 +81,9 @@ class ProjectApiTest(BaseTestCase):
         )
         self.assertEqual(get_response.status_code, HTTPStatus.NOT_FOUND)
 
-    def test_post_project(self):
+    @patch('fedlearner_webconsole.project.apis.verify_certificates')
+    def test_post_project(self, mock_verify_certificates):
+        mock_verify_certificates.return_value = (True, '')
         name = 'test-post-project'
         config = {
             'participants': [
@@ -111,6 +116,9 @@ class ProjectApiTest(BaseTestCase):
         queried_project = Project.query.filter_by(name=name).first()
         self.assertEqual(created_project, queried_project.to_dict())
 
+        mock_verify_certificates.assert_called_once_with(
+            parse_certificates(self.TEST_CERTIFICATES))
+
     def test_post_conflict_name_project(self):
         config = {
             'participants': {
@@ -142,7 +150,8 @@ class ProjectApiTest(BaseTestCase):
         project_list = json.loads(list_response.data).get('data')
         self.assertEqual(len(project_list), 1)
         for project in project_list:
-            queried_project = Project.query.filter_by(name=project['name']).first()
+            queried_project = Project.query.filter_by(
+                name=project['name']).first()
             result = queried_project.to_dict()
             result['num_workflow'] = 1
             self.assertEqual(project, result)
