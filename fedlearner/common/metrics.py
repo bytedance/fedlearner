77683957ff2e797eb374bbb758d239e800567800
ADeltanp <45765741+ZhZhang711@users.noreply.github.com>
2021-03-05 17:48:02 +0800

    multi indices (#594)
    * multi indices
    
    * add mappings in index creation
    
    * individual name for join metrics
    
    * metrics_v2
    
    * formatize tags name
    
    * flush handler at exit
    
    * minor fix on flush
    
    * fix on emit
    
    * compatible with ES 6.8
    
    * disable ES logger
    
    * more debug loggers
    
    * fix emit and add lock in ES metrics
    
    * fix event time format, add comments
    
    * standardize all emitting time format to YYYY-MM-DD`T`HH:mm:ss
    
    * fix timezone, optimize code for single index
    
    * code format, substitute negative with fake, add example id to mapping
    
    * profile purpose commit
    
    * individual index for data join
    
    * compatibility for es6 bulk, shrink down raw data sample rate
    
    * fix popping keys during iteration
    
    * comment polished
    
    * timezone placed in metrics
    
    * use configs to centralize environs
    
    * fix unused import
    
    * optimize code
    
    * ILM for ES7
    
    * fix templates
    
    * fix templates
    
    * split raw data index from metrics
    
    * fix raw data metrics sample rate
    
    * fix mappings
    
    * adjust mappings
    
    * fix raw data CI test
    
    * template for all
    
    * timezone for all date field
    
    * change date time format and add dynamic template for metrics_v2
    
    * fix for pylint and use short for partition field
    
    * raw data shell script modification
    
    * comment modifications
    
    * refactor stats using numbers
    
    * modify data join mappings
    
    * fix reversed map
    
    * fix logger emits in optional stats
    
    * fix for CI
    
    * restrain some metric tags of join stats

--- fedlearner/common/metrics.py
+++ fedlearner/common/metrics.py
@@ -14,49 +14,28 @@
 
 # coding: utf-8
 
+import atexit
+import datetime
 import logging
 import os
+import threading
 import time
-import datetime
 from functools import wraps
-import pytz
-try:
-    import thread
-    import threading
-except ImportError:
-    thread = None
 
-if thread:
-    _lock = threading.RLock()
-else:
-    _lock = None
+import elasticsearch as es7
+import elasticsearch6 as es6
+from elasticsearch import helpers as helpers7
+from elasticsearch6 import helpers as helpers6
 
-_metrics_client = None
-
-
-def _acquireLock():
-    """
-    Acquire the module-level lock for serializing access to shared data.
-
-    This should be released with _releaseLock().
-    """
-    if _lock:
-        _lock.acquire()
-
-
-def _releaseLock():
-    """
-    Release the module-level lock acquired by calling _acquireLock().
-    """
-    if _lock:
-        _lock.release()
+from fedlearner.common.common import CONFIGS, INDEX_NAME, INDEX_TYPE, \
+    convert_to_iso_format, get_es_template
 
 
 class Handler(object):
     def __init__(self, name):
         self._name = name
 
-    def emit(self, name, value, tags=None, metrics_type=None):
+    def emit(self, name, value, tags=None, index_type='metrics'):
         """
         Do whatever it takes to actually log the specified logging record.
 
@@ -69,118 +48,181 @@ class Handler(object):
     def get_name(self):
         return self._name
 
+    def flush(self):
+        pass
+
+
 class LoggingHandler(Handler):
     def __init__(self):
         super(LoggingHandler, self).__init__('logging')
 
-    def emit(self, name, value, tags=None, metrics_type=None):
+    def emit(self, name, value, tags=None, index_type='metrics'):
         logging.debug('[metrics] name[%s] value[%s] tags[%s]',
                       name, value, str(tags))
 
 
 class ElasticSearchHandler(Handler):
-    def __init__(self, ip, port):
-        from elasticsearch import Elasticsearch # pylint: disable=C0415
-        super(ElasticSearchHandler, self).__init__('elasticsearch')
-        self._es = Elasticsearch([ip], port=port)
-        self._tz = pytz.timezone('Asia/Shanghai')
-        es_logger = logging.getLogger('elasticsearch')
-        es_logger.setLevel(logging.WARNING)
-        # initialize index for elastic search
-        if self._es.indices.exists(index='metrics') is not True:
-            self._es.indices.create(index='metrics')
+    """
+    Emit documents to ElasticSearch
+    """
 
-    def emit(self, name, value, tags=None, metrics_type=None):
+    def __init__(self, ip, port):
+        super(ElasticSearchHandler, self).__init__('elasticsearch')
+        self._es = es7.Elasticsearch([ip], port=port)
+        self._helpers = helpers7
+        self._version = int(self._es.info()['version']['number'].split('.')[0])
+        # ES 6.8 has differences in APIs compared to ES 7.6,
+        # These `put_template`s is supposed to be done during deployment, here
+        # is for old clients.
+        if self._version == 6:
+            self._es = es6.Elasticsearch([ip], port=port)
+            self._helpers = helpers6
+        # first run, put index templates to ES if not exist
+        # templates are supposed to be set during deployment
+        for index_type, index_name in INDEX_NAME.items():
+            if not self._es.indices.exists_template(
+                '{}-template'.format(index_name)
+            ):
+                self._create_template_and_index(index_type)
+        # suppress ES logger
+        logging.getLogger('elasticsearch').setLevel(logging.CRITICAL)
+        self._emit_batch = []
+        self._batch_size = CONFIGS['es_batch_size']
+        self._lock = threading.RLock()
+
+    def emit(self, name, value, tags=None, index_type='metrics'):
+        assert index_type in INDEX_TYPE
         if tags is None:
             tags = {}
+        document = self._produce_document(name, value, tags, index_type)
+        action = {'_index': INDEX_NAME[index_type],
+                  '_source': document}
+        if self._version == 6:
+            action['_type'] = '_doc'
+        with self._lock:
+            # emit when there are enough documents
+            self._emit_batch.append(action)
+            if len(self._emit_batch) >= self._batch_size:
+                self.flush()
+
+    def flush(self):
+        with self._lock:
+            if len(self._emit_batch) > 0:
+                logging.info('METRICS: Emitting %d documents to ES',
+                             len(self._emit_batch))
+                self._helpers.bulk(self._es, self._emit_batch)
+                self._emit_batch = []
+
+    @staticmethod
+    def _produce_document(name, value, tags, index_type):
         application_id = os.environ.get('APPLICATION_ID', '')
         if application_id:
             tags['application_id'] = str(application_id)
-        action = {
-            "name": name,
-            "value": value,
-            "tags": tags,
-            "date_time": datetime.datetime.now(tz=self._tz)
-        }
-        self._es.index(index="metrics", body=action)
+        if index_type == 'metrics':
+            document = {
+                "name": name,
+                "value": value,
+                "tags": tags,
+                # convert to UTC+8
+                "date_time": convert_to_iso_format(datetime.datetime.now())
+            }
+        else:
+            document = tags
+        return document
+
+    def _create_template_and_index(self, index_type):
+        """
+        Args:
+            index_type: ES index type.
+
+        Creates a template and an index on ES.
+
+        """
+        assert index_type in INDEX_TYPE
+        self._es.indices.put_template(
+            name='{}-template'.format(INDEX_NAME[index_type]),
+            body=get_es_template(index_type, self._version)
+        )
+        try:
+            self._es.indices.create(index=INDEX_NAME[index_type])
+            return
+        # index may have been created by other jobs
+        except (es6.exceptions.RequestError, es7.exceptions.RequestError) as e:
+            # if due to other reasons, re-raise exception
+            if e.info['error']['type'] != 'resource_already_exists_exception':
+                raise e
 
 
 class Metrics(object):
     def __init__(self):
         self.handlers = []
+        self._lock = threading.RLock()
+        self.handler_initialized = False
+        atexit.register(self.flush_handler)
 
-    def addHandler(self, hdlr):
+    def init_handlers(self):
+        with self._lock:
+            if self.handler_initialized:
+                return
+            logging_handler = LoggingHandler()
+            self.add_handler(logging_handler)
+            es_host = os.environ.get('ES_HOST', None)
+            es_port = os.environ.get('ES_PORT', None)
+            if es_host is not None and es_port is not None:
+                es_handler = ElasticSearchHandler(es_host, es_port)
+                self.add_handler(es_handler)
+            self.handler_initialized = True
+
+    def add_handler(self, hdlr):
         """
         Add the specified handler to this logger.
         """
-        _acquireLock()
-        try:
-            if not (hdlr in self.handlers):  # pylint: disable=superfluous-parens
+        with self._lock:
+            if hdlr not in self.handlers:
                 self.handlers.append(hdlr)
-        finally:
-            _releaseLock()
 
-    def removeHandler(self, hdlr):
+    def remove_handler(self, hdlr):
         """
         Remove the specified handler from this logger.
         """
-        _acquireLock()
-        try:
+        with self._lock:
             if hdlr in self.handlers:
                 self.handlers.remove(hdlr)
-        finally:
-            _releaseLock()
 
-    def emit(self, name, value, tags=None, metrics_type=None):
+    def emit(self, name, value, tags=None, index_type='metrics'):
+        self.init_handlers()
         if not self.handlers or len(self.handlers) == 0:
-            print('no handlers. do nothing.')
+            logging.info('No handlers. Not emitting.')
             return
 
         for hdlr in self.handlers:
             try:
-                hdlr.emit(name, value, tags, metrics_type)
+                hdlr.emit(name, value, tags, index_type)
             except Exception as e:  # pylint: disable=broad-except
-                print('hdlr [%s] emit failed. [%s]' %
-                      (hdlr.get_name(), repr(e)))
+                logging.warning('Handler [%s] emit failed. Error repr: [%s]',
+                                hdlr.get_name(), repr(e))
+
+    def flush_handler(self):
+        for hdlr in self.handlers:
+            try:
+                hdlr.flush()
+            except Exception as e:  # pylint: disable=broad-except
+                logging.warning('Handler [%s] flush failed. Some metrics might '
+                                'not be emitted. Error repr: %s',
+                                hdlr.get_name(), repr(e))
 
 
-def initialize_metrics():
-    handler = LoggingHandler()
-    metrics_config(handler)
-
-    if os.environ.get('ES_HOST', None) and os.environ.get('ES_PORT', None):
-        handler = ElasticSearchHandler(
-            os.environ['ES_HOST'], os.environ['ES_PORT'])
-        metrics_config(handler)
+_metrics_client = Metrics()
 
 
-def metrics_config(handler):
-    _acquireLock()
-    global _metrics_client  # pylint: disable=global-statement
-    try:
-        if not _metrics_client:
-            _metrics_client = Metrics()
-        _metrics_client.addHandler(handler)
-    finally:
-        _releaseLock()
+def emit(name, value, tags=None, index_type='metrics'):
+    _metrics_client.emit(name, value, tags, index_type)
 
 
-def emit_counter(name, value, tags=None):
-    if not _metrics_client:
-        initialize_metrics()
-    _metrics_client.emit(name, value, tags, 'counter')
-
-
-def emit_store(name, value, tags=None):
-    if not _metrics_client:
-        initialize_metrics()
-    _metrics_client.emit(name, value, tags, 'store')
-
-
-def emit_timer(name, value, tags=None):
-    if not _metrics_client:
-        initialize_metrics()
-    _metrics_client.emit(name, value, tags, 'timer')
+# Currently no actual differences among the methods below
+emit_counter = emit
+emit_store = emit
+emit_timer = emit
 
 
 def timer(func_name, tags=None):
@@ -188,11 +230,12 @@ def timer(func_name, tags=None):
         @wraps(func)
         def return_wrapper(*args, **kwargs):
             time_start = time.time()
-
             result = func(*args, **kwargs)
             time_end = time.time()
-            time_spend = time_end-time_start
-            emit_timer(func_name, time_spend, tags)
+            time_spend = time_end - time_start
+            emit(func_name, time_spend, tags)
             return result
+
         return return_wrapper
+
     return func_wrapper
--- fedlearner/data_join/raw_data_iter_impl/metric_stats.py
+++ fedlearner/data_join/raw_data_iter_impl/metric_stats.py
@@ -1,14 +1,15 @@
 import copy
 import random
-from fedlearner.common import metrics
-from fedlearner.data_join.common import convert_to_iso_format
+
+from fedlearner.common import metrics, common
+from fedlearner.common.common import convert_to_iso_format
 
 
 class MetricStats:
     def __init__(self, raw_data_options, metric_tags):
         self._tags = copy.deepcopy(metric_tags)
         self._stat_fields = raw_data_options.optional_fields
-        self._sample_ratio = raw_data_options.input_data_stat_sample_ratio
+        self._sample_ratio = common.CONFIGS['raw_data_metrics_sample_rate']
 
     def emit_metric(self, item):
         if random.random() < self._sample_ratio:
@@ -17,8 +18,7 @@ class MetricStats:
                 value = self.convert_to_str(getattr(item, field, '#None#'))
                 tags[field] = value
             tags['example_id'] = self.convert_to_str(item.example_id)
-            tags['event_time'] = self.convert_to_str(item.event_time)
-            tags['event_time_iso'] = convert_to_iso_format(item.event_time)
+            tags['event_time'] = convert_to_iso_format(item.event_time)
             metrics.emit_store(name='input_data', value=0, tags=tags)
 
     @staticmethod
