6bdb25b7a157c8f684be5e5262291308325e623f
xiangyuxuan.prs <xiangyuxuan.prs@bytedance.com>
2021-03-09 17:20:05 +0800

    fix

--- web_console_v2/api/testing/workflow_template/psi_join_tree_model_no_label.py
+++ web_console_v2/api/testing/workflow_template/psi_join_tree_model_no_label.py
@@ -114,11 +114,11 @@ def make_workflow_template():
                                     },
                                     {
                                         "name": "EGRESS_HOST",
-                                        "value": "${project.participants.egress_host}"
+                                        "value": "${project.participants.0.egress_host}"
                                     },
                                     {
                                         "name": "EGRESS_DOMAIN",
-                                        "value": "${project.participants.egress_domain}"
+                                        "value": "${project.participants.0.egress_domain}"
                                     },
                                     {
                                         "name": "STORAGE_ROOT_PATH",
@@ -215,11 +215,11 @@ def make_workflow_template():
                                     },
                                     {
                                         "name": "EGRESS_HOST",
-                                        "value": "${project.participants.egress_host}"
+                                        "value": "${project.participants.0.egress_host}"
                                     },
                                     {
                                         "name": "EGRESS_DOMAIN",
-                                        "value": "${project.participants.egress_domain}"
+                                        "value": "${project.participants.0.egress_domain}"
                                     },
                                     {
                                         "name": "STORAGE_ROOT_PATH",
@@ -315,7 +315,7 @@ def make_workflow_template():
         "peerSpecs": {
             "Follower": {
                 "peerURL": "fedlearner-stack-ingress-nginx-controller.default.svc.cluster.local:80",
-                "authority": "${project.participants.egress_domain}",
+                "authority": "${project.participants.0.egress_domain}",
                 "extraHeaders": {
                     "x-host": "default.fedlearner.operator"
                 }
@@ -336,11 +336,11 @@ def make_workflow_template():
                                     },
                                     {
                                         "name": "EGRESS_HOST",
-                                        "value": "${project.participants.egress_host}"
+                                        "value": "${project.participants.0.egress_host}"
                                     },
                                     {
                                         "name": "EGRESS_DOMAIN",
-                                        "value": "${project.participants.egress_domain}"
+                                        "value": "${project.participants.0.egress_domain}"
                                     },
                                     {
                                         "name": "APPLICATION_ID",
@@ -444,11 +444,11 @@ def make_workflow_template():
                                     },
                                     {
                                         "name": "EGRESS_HOST",
-                                        "value": "${project.participants.egress_host}"
+                                        "value": "${project.participants.0.egress_host}"
                                     },
                                     {
                                         "name": "EGRESS_DOMAIN",
-                                        "value": "${project.participants.egress_domain}"
+                                        "value": "${project.participants.0.egress_domain}"
                                     },
                                     {
                                         "name": "STORAGE_ROOT_PATH",
@@ -607,7 +607,7 @@ def make_workflow_template():
         "peerSpecs": {
             "Leader": {
                 "peerURL": "fedlearner-stack-ingress-nginx-controller.default.svc.cluster.local:80",
-                "authority": "${project.participants.egress_domain}",
+                "authority": "${project.participants.0.egress_domain}",
                 "extraHeaders": {
                     "x-host": "default.fedlearner.operator"
                 }
@@ -628,11 +628,11 @@ def make_workflow_template():
                                     },
                                     {
                                         "name": "EGRESS_HOST",
-                                        "value": "${project.participants.egress_host}"
+                                        "value": "${project.participants.0.egress_host}"
                                     },
                                     {
                                         "name": "EGRESS_DOMAIN",
-                                        "value": "${project.participants.egress_domain}"
+                                        "value": "${project.participants.0.egress_domain}"
                                     },
                                     {
                                         "name": "APPLICATION_ID",
