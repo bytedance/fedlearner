bb072ab0605166ff21d3369f4f8ca7e2b34b5e91
xiangyuxuan.prs <xiangyuxuan.prs@bytedance.com>
2021-03-14 18:40:28 +0800

    add PUT template api

--- web_console_v2/api/fedlearner_webconsole/workflow_template/apis.py
+++ web_console_v2/api/fedlearner_webconsole/workflow_template/apis.py
@@ -67,36 +67,12 @@ class WorkflowTemplatesApi(Resource):
         name = data['name']
         comment = data['comment']
         config = data['config']
-        # TODO: format check
-        if 'group_alias' not in config:
-            raise InvalidArgumentException(details={
-                'config.group_alias': 'config.group_alias is required'})
-        if 'is_left' not in config:
-            raise InvalidArgumentException(
-                details={'config.is_left': 'config.is_left is required'})
-
         if WorkflowTemplate.query.filter_by(name=name).first() is not None:
             raise ResourceConflictException(
                 'Workflow template {} already exists'.format(name))
-        # form to proto buffer
+        _check_config(config)
         template_proto = dict_to_workflow_definition(config)
-        for index, job_def in enumerate(template_proto.job_definitions):
-            # pod label name must be no more than 63 characters.
-            #  workflow.uuid is 20 characters, pod name suffix such as
-            #  '-follower-master-0' is less than 19 characters, so the
-            #  job name must be no more than 24
-            if len(job_def.name) > 24:
-                raise InvalidArgumentException(
-                    details=
-                    {'config.job_definitions'
-                     : 'job_name:{} must be no more than 24 characters'})
-            # limit from k8s
-            if not re.match('[a-z0-9-]*', job_def.name):
-                raise InvalidArgumentException(
-                    details=
-                    {f'config.job_definitions[{index}].job_name'
-                     : 'Only letters(a-z), numbers(0-9) '
-                       'and dashes(-) are supported.'})
+
         template = WorkflowTemplate(name=name,
                                     comment=comment,
                                     group_alias=template_proto.group_alias,
@@ -122,6 +98,63 @@ class WorkflowTemplateApi(Resource):
         result.delete()
         db.session.commit()
         return {'data': {}}, HTTPStatus.OK
+
+    def put(self, template_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required=True, help='name is empty')
+        parser.add_argument('comment')
+        parser.add_argument('config', type=dict, required=True,
+                            help='config is empty')
+        data = parser.parse_args()
+        name = data['name']
+        comment = data['comment']
+        config = data['config']
+        if WorkflowTemplate.query.filter_by(name=name).first() is not None:
+            raise ResourceConflictException(
+                'Workflow template {} already exists'.format(name))
+        template = WorkflowTemplate.query.filter_by(id=template_id).first()
+        if template is None:
+            raise NotFoundException()
+        _check_config(config)
+        template_proto = dict_to_workflow_definition(config)
+
+        template.set_config(template_proto)
+        template.name = name
+        template.comment = comment
+        template.group_alias = template_proto.group_alias
+        template.is_left = template_proto.is_left
+
+
+def _check_config(config):
+    # TODO: format check
+    if 'group_alias' not in config:
+        raise InvalidArgumentException(details={
+            'config.group_alias': 'config.group_alias is required'})
+    if 'is_left' not in config:
+        raise InvalidArgumentException(
+            details={'config.is_left': 'config.is_left is required'})
+
+    # form to proto buffer
+    template_proto = dict_to_workflow_definition(config)
+    for index, job_def in enumerate(template_proto.job_definitions):
+        # pod label name must be no more than 63 characters.
+        #  workflow.uuid is 20 characters, pod name suffix such as
+        #  '-follower-master-0' is less than 19 characters, so the
+        #  job name must be no more than 24
+        if len(job_def.name) > 24:
+            raise InvalidArgumentException(
+                details=
+                {'config.job_definitions'
+                 : 'job_name:{} must be no more than 24 characters'})
+        # limit from k8s
+        if not re.match('[a-z0-9-]*', job_def.name):
+            raise InvalidArgumentException(
+                details=
+                {f'config.job_definitions[{index}].job_name'
+                 : 'Only letters(a-z), numbers(0-9) '
+                   'and dashes(-) are supported.'})
+
+
 
 def initialize_workflow_template_apis(api):
     api.add_resource(WorkflowTemplatesApi, '/workflow_templates')
