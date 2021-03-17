8bf7f77b17add82b092bb0c7542b1a23b02f43e3
zhangzihui <zhangzihui@bytedance.com>
2021-03-17 17:43:11 +0800

    fix for CI

--- web_console_v2/api/fedlearner_webconsole/envs.py
+++ web_console_v2/api/fedlearner_webconsole/envs.py
@@ -2,14 +2,14 @@ import os
 
 import pytz
 
-SUPPORT_HDFS = bool(os.getenv('SUPPORT_HDFS'))
+SUPPORT_HDFS = bool(os.environ.get('SUPPORT_HDFS'))
 TZ = pytz.timezone(os.environ.get('TZ', 'UTC'))
-HDFS_SERVER = os.getenv('HDFS_SERVER', None)
-ES_HOST = os.getenv('ES_HOST', 'fedlearner-stack-elasticsearch-client')
-ES_PORT = os.getenv('ES_PORT', 9200)
-ES_USERNAME = os.getenv('ES_USERNAME', 'elastic')
-ES_PASSWORD = os.getenv('ES_PASSWORD', 'Fedlearner123')
-KIBANA_SERVICE_HOST_PORT = os.getenv('KIBANA_SERVICE_HOST_PORT',
-                                     'http://fedlearner-stack-kibana:443')
-KIBANA_INGRESS_HOST = os.getenv('KIBANA_INGRESS_HOST', 'localhost')
-KIBANA_INGRESS_PORT = os.getenv('KIBANA_INGRESS_PORT', '5601')
+HDFS_SERVER = os.environ.get('HDFS_SERVER', None)
+ES_HOST = os.environ.get('ES_HOST', 'fedlearner-stack-elasticsearch-client')
+ES_PORT = os.environ.get('ES_PORT', 9200)
+ES_USERNAME = os.environ.get('ES_USERNAME', 'elastic')
+ES_PASSWORD = os.environ.get('ES_PASSWORD', 'Fedlearner123')
+KIBANA_SERVICE_HOST_PORT = os.environ.get('KIBANA_SERVICE_HOST_PORT',
+                                          'http://fedlearner-stack-kibana:443')
+KIBANA_INGRESS_HOST = os.environ.get('KIBANA_INGRESS_HOST', 'localhost')
+KIBANA_INGRESS_PORT = os.environ.get('KIBANA_INGRESS_PORT', '5601')
