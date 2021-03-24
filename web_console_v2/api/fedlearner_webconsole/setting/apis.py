2c9906102a99fee84946de5f515bef77e3d40679
Fan Lin (林凡) <linfan.fine@bytedance.com>
2021-03-24 16:45:59 +0800

    [WebConsoleV2] Gets web console image with namespace in settings API.

--- web_console_v2/api/fedlearner_webconsole/setting/apis.py
+++ web_console_v2/api/fedlearner_webconsole/setting/apis.py
@@ -13,21 +13,33 @@
 # limitations under the License.
 
 # coding: utf-8
-# pylint: disable=raise-missing-from
+from pathlib import Path
 
 from flask_restful import Resource, reqparse
 from fedlearner_webconsole.k8s_client import get_client
 
+_POD_NAMESPACE = 'default'
+# Ref: https://stackoverflow.com/questions/46046110/
+#      how-to-get-the-current-namespace-in-a-pod
+_k8s_namespace_file = Path(
+    '/var/run/secrets/kubernetes.io/serviceaccount/namespace')
+if _k8s_namespace_file.is_file():
+    _POD_NAMESPACE = _k8s_namespace_file.read_text()
+
+
 class SettingsApi(Resource):
     def get(self):
-        res = {}
-
         k8s_client = get_client()
-        deploy = k8s_client.get_deployment(
-            'fedlearner-web-console-v2')
-        res['webconsole_image'] = deploy.spec.template.spec.containers[0].image
+        deployment = k8s_client.get_deployment(
+            name='fedlearner-web-console-v2',
+            namespace=_POD_NAMESPACE)
 
-        return {'data': res}
+        return {
+            'data': {
+                'webconsole_image':
+                    deployment.spec.template.spec.containers[0].image
+            }
+        }
 
     def patch(self):
         parser = reqparse.RequestParser()
@@ -36,13 +48,19 @@ class SettingsApi(Resource):
         data = parser.parse_args()
 
         if data['webconsole_image']:
+            new_image = data['webconsole_image']
             k8s_client = get_client()
-            deploy = k8s_client.get_deployment(
-                'fedlearner-web-console-v2')
-            deploy.spec.template.spec.containers[0].image = \
-                data['webconsole_image']
+            deployment = k8s_client.get_deployment(
+                'fedlearner-web-console-v2',
+                _POD_NAMESPACE)
+            spec = deployment.spec
+            spec.template.spec.containers[0].image = new_image
+            metadata = deployment.metadata
             k8s_client.create_or_update_deployment(
-                deploy.metadata, deploy.spec, 'fedlearner-web-console-v2')
+                metadata=metadata,
+                spec=spec,
+                name=metadata.name,
+                namespace=metadata.namespace)
 
         return {'data': {}}
 
---
+++ web_console_v2/api/test/fedlearner_webconsole/setting/apis_test.py
@@ -0,0 +1,85 @@
+# Copyright 2020 The FedLearner Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+import logging
+import unittest
+from http import HTTPStatus
+from types import SimpleNamespace
+from unittest.mock import patch, MagicMock
+
+from testing.common import BaseTestCase
+
+
+class SettingsApiTest(BaseTestCase):
+    class Config(BaseTestCase.Config):
+        START_GRPC_SERVER = False
+        START_SCHEDULER = False
+
+    def setUp(self):
+        super().setUp()
+        self._deployment = SimpleNamespace(**{
+            'metadata': SimpleNamespace(**{
+                'name': 'fedlearner-web-console-v2',
+                'namespace': 'testns'
+            }),
+            'spec': SimpleNamespace(**{
+                'template': SimpleNamespace(**{
+                    'spec': SimpleNamespace(**{
+                        'containers': [SimpleNamespace(**{
+                            'image': 'fedlearner:test'
+                        })]
+                    })
+                })
+            })
+        })
+        self._mock_k8s_client = MagicMock()
+        self._mock_k8s_client.get_deployment = MagicMock(
+            return_value=self._deployment)
+
+    @patch('fedlearner_webconsole.setting.apis._POD_NAMESPACE', 'testns')
+    def test_get_settings(self):
+        with patch('fedlearner_webconsole.setting.apis.get_client',
+                   return_value=self._mock_k8s_client):
+            response_data = self.get_response_data(
+                self.get_helper('/api/v2/settings'))
+            self.assertEqual(response_data, {
+                'webconsole_image': 'fedlearner:test'
+            })
+            self._mock_k8s_client.get_deployment.assert_called_with(
+                name='fedlearner-web-console-v2',
+                namespace='testns'
+            )
+
+    def test_update_image(self):
+        self._mock_k8s_client.create_or_update_deployment = MagicMock()
+        with patch('fedlearner_webconsole.setting.apis.get_client',
+                   return_value=self._mock_k8s_client):
+            resp = self.patch_helper(
+                '/api/v2/settings',
+                data={
+                    'webconsole_image': 'test-new-image'
+                })
+            self.assertEqual(resp.status_code, HTTPStatus.OK)
+            _, kwargs = self._mock_k8s_client.create_or_update_deployment.call_args
+            self.assertEqual(kwargs['spec'].template.spec.containers[0].image,
+                             'test-new-image')
+            self.assertEqual(kwargs['name'], self._deployment.metadata.name)
+            self.assertEqual(kwargs['namespace'],
+                             self._deployment.metadata.namespace)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
