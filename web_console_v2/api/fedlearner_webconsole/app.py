11d452dc0c7afa0b448dafeac3ad4d896a3e4655
revin <caoyunbin001@126.com>
2021-03-25 14:48:20 +0800

    [mmgr] add api

--- web_console_v2/api/fedlearner_webconsole/app.py
+++ web_console_v2/api/fedlearner_webconsole/app.py
@@ -36,6 +36,7 @@ from fedlearner_webconsole.workflow.apis import initialize_workflow_apis
 from fedlearner_webconsole.dataset.apis import initialize_dataset_apis
 from fedlearner_webconsole.job.apis import initialize_job_apis
 from fedlearner_webconsole.setting.apis import initialize_setting_apis
+from fedlearner_webconsole.mmgr.apis import initialize_mmgr_apis
 from fedlearner_webconsole.rpc.server import rpc_server
 from fedlearner_webconsole.db import db
 from fedlearner_webconsole.exceptions import (
@@ -140,6 +141,7 @@ def create_app(config):
     initialize_job_apis(api)
     initialize_dataset_apis(api)
     initialize_setting_apis(api)
+    initialize_mmgr_apis(api)
     # A hack that use our customized error handlers
     # Ref: https://github.com/flask-restful/flask-restful/issues/280
     handle_exception = app.handle_exception
---
+++ web_console_v2/api/fedlearner_webconsole/mmgr/apis.py
@@ -0,0 +1,41 @@
+# Copyright 2020 The FedLearner Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+# pylint: disable=cyclic-import
+
+import functools
+from http import HTTPStatus
+from flask import request
+from flask_restful import Resource, reqparse
+from flask_jwt_extended.utils import get_current_user
+from flask_jwt_extended import jwt_required, create_access_token
+
+from fedlearner_webconsole.db import db
+from fedlearner_webconsole.auth.models import (State, User, Role,
+                                               MUTABLE_ATTRS_MAPPER)
+from fedlearner_webconsole.exceptions import (NotFoundException,
+                                              InvalidArgumentException,
+                                              ResourceConflictException,
+                                              UnauthorizedException)
+
+class ModelApi(Resource):
+    def post(self, op):
+        return {
+            "op": op
+        }, HTTPStatus.OK
+
+def initialize_mmgr_apis(api):
+    api.add_resource(ModelApi, '/model/<string:op>')
+
