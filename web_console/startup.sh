cedeb0faa03c6aae2706873a3e0acc93fea25a11
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2021-04-23 15:23:57 +0800

    Escape DB password for webconsole V1 (#807)

--- web_console/startup.sh
+++ web_console/startup.sh
@@ -2,8 +2,28 @@
 
 set -ex
 
+urlencode() {
+    # urlencode <string>
+
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:$i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+
+    LC_COLLATE=$old_lc_collate
+}
+
+ENCODE_DB_PASSWORD=`urlencode ${DB_PASSWORD:-fedlearner}`
+
 npx sequelize-cli db:migrate \
-    --url="${DB_DIALECT:-mysql}://${DB_USERNAME:-fedlearner}:${DB_PASSWORD:-fedlearner}@${DB_HOST:-127.0.0.1}:${DB_PORT:-3306}/${DB_DATABASE:-fedlearner}"
+    --url="${DB_DIALECT:-mysql}://${DB_USERNAME:-fedlearner}:${ENCODE_DB_PASSWORD}@${DB_HOST:-127.0.0.1}:${DB_PORT:-3306}/${DB_DATABASE:-fedlearner}"
 
 node bootstrap.js &
 
