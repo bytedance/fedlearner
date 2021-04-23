bd5c2a8abbef01bb14fd91ef82401d9a22d0315f
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2021-04-23 16:04:48 +0800

    fix (#808)

--- web_console/startup.sh
+++ web_console/startup.sh
@@ -9,12 +9,14 @@ urlencode() {
     LC_COLLATE=C
 
     local length="${#1}"
-    for (( i = 0; i < length; i++ )); do
+    i=0
+    while [ "$i" -lt "$length" ]; do
         local c="${1:$i:1}"
         case $c in
             [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
             *) printf '%%%02X' "'$c" ;;
         esac
+        i=$(( i + 1 ))
     done
 
     LC_COLLATE=$old_lc_collate
