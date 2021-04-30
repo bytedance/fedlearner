2f3d74e92586bc7b4c8c87c9d72f5a0c6b86418e
Bing <hibduan@gmail.com>
2021-05-01 01:54:56 +0800

    Add nightly release (#813)
    * Add nightly release
    
    * Update py version
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Add nightly release
    
    * Fedlearner
    
    * Add night release update
    
    * Add night release update
    
    * Add night release update

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
+import os
+import platform
 from setuptools import setup, find_packages
+import time
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+def get_version():
+    base = "1.5"
+    day = time.strftime('%Y%m%d', time.localtime())
+    return '%s-dev%s'%(base, day)
 
 setup(
     name='fedlearner',
-    version='0.1',
+    version=get_version(),
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'click'
-    ],
-    entry_points='''
-        [console_scripts]
-        fedlearner=fedlearner.cli.cli:cli_group
-    ''',
+    author='Fedlearner Contributors',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=required,
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: Apache Software License",
+         "Operating System :: MacOS",
+         "Operating System :: POSIX :: Linux"
+     ],
 )
