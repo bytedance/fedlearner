424af713db687801c661218253fab5ccedc1f1a5
liuqi <liuqi.nolan@bytedance.com>
2021-05-28 17:02:28 +0800

    fix shuffle in day.

--- deploy/scripts/trainer/run_trainer_master.sh
+++ deploy/scripts/trainer/run_trainer_master.sh
@@ -31,7 +31,7 @@ epoch_num=$(normalize_env_to_args "--epoch-num" $EPOCH_NUM)
 start_date=$(normalize_env_to_args "--start-date" $START_DATE)
 end_date=$(normalize_env_to_args "--end-date" $END_DATE)
 shuffle=$(normalize_env_to_args "--shuffle" $SUFFLE_DATA_BLOCK)
-shuffle_in_day=$(normalize_env_to_args "--shuffle_in_day" $SHUFFLE_IN_DAY)
+shuffle_in_day=$(normalize_env_to_args "--shuffle-in-day" $SHUFFLE_IN_DAY)
 local_data_source=$(normalize_env_to_args "--local-data-source" $LOCAL_DATA_SOURCE)
 local_start_date=$(normalize_env_to_args "--local-start-date" $LOCAL_START_DATE)
 local_end_date=$(normalize_env_to_args "--local-end-date" $LOCAL_END_DATE)
@@ -91,4 +91,4 @@ python main.py --master \
     $save_checkpoint_steps $save_checkpoint_secs \
     $summary_save_steps $summary_save_secs \
     $local_data_source $local_start_date $local_end_date \
-    $epoch_num $start_date $end_date $SHUFFLE $SHUFFLE_IN_DAY
+    $epoch_num $start_date $end_date $shuffle $shuffle_in_day
