1da726593cb1a2634dc2349b8b1ed48281c47e72
liuqi <liuqi.nolan@bytedance.com>
2021-05-28 15:36:42 +0800

    polish

--- deploy/scripts/trainer/run_trainer_master.sh
+++ deploy/scripts/trainer/run_trainer_master.sh
@@ -30,8 +30,6 @@ summary_save_secs=$(normalize_env_to_args "--summary-save-secs" "$SUMMARY_SAVE_S
 epoch_num=$(normalize_env_to_args "--epoch-num" $EPOCH_NUM)
 start_date=$(normalize_env_to_args "--start-date" $START_DATE)
 end_date=$(normalize_env_to_args "--end-date" $END_DATE)
-shuffle=$(normalize_env_to_args "--shuffle" $SUFFLE_DATA_BLOCK)
-shuffle_in_day=$(normalize_env_to_args "--shuffle-in-day" $SHUFFLE_IN_DAY)
 local_data_source=$(normalize_env_to_args "--local-data-source" $LOCAL_DATA_SOURCE)
 local_start_date=$(normalize_env_to_args "--local-start-date" $LOCAL_START_DATE)
 local_end_date=$(normalize_env_to_args "--local-end-date" $LOCAL_END_DATE)
@@ -91,4 +89,4 @@ python main.py --master \
     $save_checkpoint_steps $save_checkpoint_secs \
     $summary_save_steps $summary_save_secs \
     $local_data_source $local_start_date $local_end_date \
-    $epoch_num $start_date $end_date $shuffle $shuffle_in_day
+    $epoch_num $start_date $end_date $SHUFFLE $SHUFFLE_IN_DAY
