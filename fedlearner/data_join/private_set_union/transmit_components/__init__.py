a9de476aa46e0a63d82a2959e29e5aa2f0798c74
zhangzihui <zhangzihui@bytedance.com>
2021-06-10 14:50:14 +0800

    refactor(psu): __init__ in components

---
+++ fedlearner/data_join/private_set_union/transmit_components/__init__.py
@@ -0,0 +1,5 @@
+from fedlearner.data_join.private_set_union.transmit_components \
+    .parquet_encrypt_components import \
+    ParquetEncryptSender, ParquetEncryptReceiver
+from fedlearner.data_join.private_set_union.transmit_components \
+    .parquet_sync_components import ParquetSyncSender, ParquetSyncReceiver
