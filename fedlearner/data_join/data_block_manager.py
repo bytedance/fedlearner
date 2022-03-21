7993ff483d121e2cfc5f585114fa14b55474b3e1
liuqi <liuqi.nolan@bytedance.com>
2022-03-21 14:18:51 +0800

    feat: add debug log for stream joiner

--- fedlearner/data_join/data_block_manager.py
+++ fedlearner/data_join/data_block_manager.py
@@ -67,6 +67,9 @@ class DataBlockBuilder(object):
         example_id = item.example_id
         if event_time is None:
             event_time = item.event_time
+        logging.info("***** append item leader_index %d, follower_index%d, "
+                     "eid %s, event time %s", leader_index, follower_index,
+                     example_id, str(event_time))
         self._data_block_meta.example_ids.append(example_id)
         if hasattr(item, 'id_type'):
             # v2
@@ -259,6 +262,8 @@ class DataBlockManager(object):
     def get_lastest_data_block_meta(self):
         with self._lock:
             self._sync_dumped_index()
+            logging.info("***** latest dumped data block meta index %d"
+                         , self._dumped_index)
             return self._sync_data_block_meta(self._dumped_index)
 
     def commit_data_block_meta(self, tmp_meta_fpath, data_block_meta):
@@ -284,6 +289,8 @@ class DataBlockManager(object):
 
     def _sync_dumped_index(self):
         if self._dumped_index is None:
+            logging.info("***** data block manager dumped index %d",
+                         self._dumped_index)
             assert self._dumping_index is None, \
                 "no index is dumping when no dumped index"
             left_index = 0
@@ -297,6 +304,8 @@ class DataBlockManager(object):
                     right_index = index - 1
             self._dumped_index = right_index
         elif self._dumping_index is not None:
+            logging.info("***** data block manager dumped index %d",
+                         self._dumping_index)
             assert self._dumping_index == self._dumped_index + 1, \
                 "the dumping index shoud be next of dumped index "\
                 "{} != {} + 1".format(self._dumping_index, self._dumped_index)
