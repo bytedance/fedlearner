71f11104881b9c2686f7b5fd53c6c5b673c443db
杭卫强 <hangweiqiang@bytedance.com>
2022-04-25 00:02:37 +0800

    chore(nn): adapt to hostnetwork

--- deploy/scripts/trainer/run_fedavg.sh
+++ deploy/scripts/trainer/run_fedavg.sh
@@ -8,8 +8,13 @@ if [[ -n "${CODE_KEY}" ]]; then
   pull_code ${CODE_KEY} $PWD
 fi
 
+LISTEN_PORT=50051
+if [[ -n "${PORT0}" ]]; then
+  LISTEN_PORT=${PORT0}
+fi
+
 if [[ $ROLE == "leader" ]]; then
-  export FL_LEADER_ADDRESS="0.0.0.0:50051"
+  export FL_LEADER_ADDRESS="0.0.0.0:${LISTEN_PORT}"
 elif [[ -n $PEER_ADDR ]]; then
   export FL_LEADER_ADDRESS=$PEER_ADDR
 fi
