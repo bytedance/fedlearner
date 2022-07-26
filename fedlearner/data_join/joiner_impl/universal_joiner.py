48024b84ec2464ac2746ef51911b531b6c40403c
liuqi <liuqi.nolan@bytedance.com>
2022-07-26 21:18:40 +0800

    fix(joiner): dead lock for universal joiner

--- fedlearner/data_join/joiner_impl/universal_joiner.py
+++ fedlearner/data_join/joiner_impl/universal_joiner.py
@@ -407,7 +407,6 @@ class UniversalJoiner(ExampleJoiner):
             return
         sync_example_id_finished, raw_data_finished = \
                 self._prepare_join(state_stale)
-        join_data_finished = False
 
         while True:
             fill_leader_enough = self._fill_leader_join_window(
@@ -479,6 +478,7 @@ class UniversalJoiner(ExampleJoiner):
                 break
 
             if self._leader_join_window.is_full() or not fill_leader_enough:
+                join_data_finished = True
                 break
 
         if self._get_data_block_builder(False) is not None and \
