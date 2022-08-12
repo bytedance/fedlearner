f744d56ecf14f6b3e4d29c37b44adcd4354de2db
杭卫强 <hangweiqiang@bytedance.com>
2022-08-12 10:51:15 +0800

    feat(tree): add file_wildcard to DataBlockLoader

--- fedlearner/model/tree/trainer.py
+++ fedlearner/model/tree/trainer.py
@@ -405,9 +405,10 @@ def test_one_file(args, bridge, booster, data_file, output_file):
 
 
 class DataBlockLoader(object):
-    def __init__(self, role: str, bridge: Optional[Bridge], data_path: str, ext: Optional[str],
-                 file_wildcard: Optional[str], worker_rank: int = 0,
-                 num_workers: int = 1, output_path: Optional[str] = None):
+    def __init__(self, role: str, bridge: Optional[Bridge], data_path: str,
+                 ext: Optional[str], file_wildcard: Optional[str],
+                 worker_rank: int = 0, num_workers: int = 1,
+                 output_path: Optional[str] = None):
         self._role = role
         self._bridge = bridge
         self._num_workers = num_workers
