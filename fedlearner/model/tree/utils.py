9b06b80199aa4538b070d2784d34502113e582a3
gejielun <18243586937@163.com>
2022-09-01 15:21:43 +0800

    fix(tree): fix filter files error

--- fedlearner/model/tree/utils.py
+++ fedlearner/model/tree/utils.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from fnmatch import fnmatch
 from typing import List, Optional
 
@@ -8,14 +9,16 @@ import tensorflow.compat.v1 as tf
 def filter_files(path: str, file_ext: Optional[str],
                  file_wildcard: Optional[str]) -> List[str]:
     files = []
-    for dirname, _, filenames in tf.io.gfile.walk(path):
-        for filename in filenames:
-            _, ext = os.path.splitext(filename)
-            subdirname = os.path.join(path, os.path.relpath(dirname, path))
-            fpath = os.path.join(subdirname, filename)
-            if file_ext and ext != file_ext:
-                continue
-            if file_wildcard and not fnmatch(fpath, file_wildcard):
-                continue
-            files.append(fpath)
+    filenames = tf.io.gfile.listdir(path)
+    for filename in filenames:
+        _, ext = os.path.splitext(filename)
+        fpath = os.path.join(path, filename)
+        if tf.io.gfile.isdir(fpath):
+            continue
+        if file_ext and ext != file_ext:
+            continue
+        if file_wildcard and not fnmatch(fpath, file_wildcard):
+            continue
+        files.append(fpath)
+    logging.info("filtered files: %s", files)
     return files
