ba95d4dcb95f5f7bad1f264298d3df4cf8ec699d
gejielun <18243586937@163.com>
2022-09-08 12:02:14 +0800

    fix(tree): fix filter files error

--- fedlearner/model/tree/utils.py
+++ fedlearner/model/tree/utils.py
@@ -9,17 +9,21 @@ import tensorflow.compat.v1 as tf
 def filter_files(path: str, file_ext: Optional[str],
                  file_wildcard: Optional[str]) -> List[str]:
     files = []
-    filenames = tf.io.gfile.listdir(path)
-    for filename in filenames:
-        _, ext = os.path.splitext(filename)
-        fpath = os.path.join(path, filename)
-        if tf.io.gfile.isdir(fpath):
-            continue
-        if file_ext and ext != file_ext:
-            continue
-        if file_wildcard and not fnmatch(fpath, file_wildcard):
-            continue
-        files.append(fpath)
-    logging.info("file wildcard is %s, filtered files"
-                 " num: %d", file_wildcard, len(files))
+    depth = 0
+    for dirname, _, filenames in tf.io.gfile.walk(path):
+        if depth == 2:
+            break
+        for filename in filenames:
+            _, ext = os.path.splitext(filename)
+            subdirname = os.path.join(path, os.path.relpath(dirname, path))
+            fpath = os.path.join(subdirname, filename)
+            if file_ext and file_ext != ext:
+                continue
+            if file_wildcard and not fnmatch(fpath, file_wildcard):
+                continue
+            files.append(fpath)
+        depth += 1
+    logging.info("file wildcard is %s, file ext is %s, "
+                 "filtered files num: %d", file_wildcard,
+                 file_ext, len(files))
     return files
--- test/tree_model/test_filter_files.py
+++ test/tree_model/test_filter_files.py
@@ -1,3 +1,4 @@
+import os.path
 import tempfile
 import logging
 import unittest
@@ -10,24 +11,29 @@ class TestFilterFiles(unittest.TestCase):
         path = tempfile.mkdtemp()
         path = Path(path, 'test').resolve()
         path.mkdir()
-        path.joinpath('test1').mkdir()
+        path.joinpath('sub_test').mkdir()
         path.joinpath('1.csv').touch()
         path.joinpath('2.csv').touch()
         path.joinpath('3.csv').touch()
         path.joinpath('1.tfrecord').touch()
         path.joinpath('2.tfrecord').touch()
         path.joinpath('3.tfrecord').touch()
-        path.joinpath('test1').joinpath('4.csv').touch()
-        path.joinpath('test1').joinpath('4.tfrecord').touch()
+        path.joinpath('sub_test').joinpath('sub_sub_test').mkdir()
+        path.joinpath('sub_test').joinpath('4.csv').touch()
+        path.joinpath('sub_test').joinpath('4.tfrecord').touch()
+        path.joinpath('sub_test').joinpath('sub_sub_test').joinpath('5.csv').touch()
+        path.joinpath('sub_test').joinpath('sub_sub_test').joinpath('5.tfrecord').touch()
 
         files = filter_files(str(path), '.csv', '')
-        self.assertEqual(len(files), 3)
+        self.assertEqual(len(files), 4)
         files = filter_files(str(path), '', '*tfr*')
-        self.assertEqual(len(files), 3)
+        self.assertEqual(len(files), 4)
         files = filter_files(str(path), '', '')
-        self.assertEqual(len(files), 6)
+        self.assertEqual(len(files), 8)
         files = filter_files(str(path), '.csv', '*1.*')
         self.assertEqual(len(files), 1)
+        files = filter_files((str(os.path.join(path, 'sub_test'))), '', '*csv')
+        self.assertEqual(len(files), 2)
 
 
 if __name__ == '__main__':
