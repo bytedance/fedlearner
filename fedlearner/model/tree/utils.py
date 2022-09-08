b250b49c50ea1ee2c88b3c575f08001550284aae
gejielun <18243586937@163.com>
2022-09-08 12:03:57 +0800

    fix(tree): fix filter files error

--- fedlearner/model/tree/utils.py
+++ fedlearner/model/tree/utils.py
@@ -17,7 +17,7 @@ def filter_files(path: str, file_ext: Optional[str],
             _, ext = os.path.splitext(filename)
             subdirname = os.path.join(path, os.path.relpath(dirname, path))
             fpath = os.path.join(subdirname, filename)
-            if file_ext and file_ext != ext:
+            if file_ext and ext != file_ext:
                 continue
             if file_wildcard and not fnmatch(fpath, file_wildcard):
                 continue
