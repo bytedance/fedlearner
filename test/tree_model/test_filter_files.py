931894492381477fb83eaca4c7ba81b3831f9107
gejielun <45250189+gejielun@users.noreply.github.com>
2022-09-08 22:41:06 +0800

    fix(tree): fix filter files error (#1046)

--- test/tree_model/test_filter_files.py
+++ test/tree_model/test_filter_files.py
@@ -1,4 +1,6 @@
+import os.path
 import tempfile
+import logging
 import unittest
 from pathlib import Path
 from fedlearner.model.tree.trainer import filter_files
@@ -9,28 +11,31 @@ class TestFilterFiles(unittest.TestCase):
         path = tempfile.mkdtemp()
         path = Path(path, 'test').resolve()
         path.mkdir()
-        path.joinpath('test1').mkdir()
-        path.joinpath('test2').mkdir()
+        path.joinpath('sub_test').mkdir()
+        path.joinpath('1.csv').touch()
+        path.joinpath('2.csv').touch()
         path.joinpath('3.csv').touch()
+        path.joinpath('1.tfrecord').touch()
+        path.joinpath('2.tfrecord').touch()
         path.joinpath('3.tfrecord').touch()
-        path.joinpath('test1').joinpath('1.csv').touch()
-        path.joinpath('test1').joinpath('2.tfrecord').touch()
-        path.joinpath('test2').joinpath('2.csv').touch()
-        path.joinpath('test2').joinpath('1.tfrecord').touch()
-        path.joinpath('test1/test').mkdir()
-        path.joinpath('test1/test').joinpath('4.csv').touch()
-        path.joinpath('test2/test').mkdir()
-        path.joinpath('test2/test').joinpath('4.tfrecord').touch()
-        
-        files = filter_files(path, '.csv', '')
+        path.joinpath('sub_test').joinpath('sub_sub_test').mkdir()
+        path.joinpath('sub_test').joinpath('4.csv').touch()
+        path.joinpath('sub_test').joinpath('4.tfrecord').touch()
+        path.joinpath('sub_test').joinpath('sub_sub_test').joinpath('5.csv').touch()
+        path.joinpath('sub_test').joinpath('sub_sub_test').joinpath('5.tfrecord').touch()
+
+        files = filter_files(str(path), '.csv', '')
         self.assertEqual(len(files), 4)
-        files = filter_files(path, '', '*tfr*')
+        files = filter_files(str(path), '', '*tfr*')
         self.assertEqual(len(files), 4)
-        files = filter_files(path, '', '')
+        files = filter_files(str(path), '', '')
         self.assertEqual(len(files), 8)
-        files = filter_files(path, '.csv', '*1.*')
+        files = filter_files(str(path), '.csv', '*1.*')
         self.assertEqual(len(files), 1)
+        files = filter_files((str(os.path.join(path, 'sub_test'))), '', '*csv')
+        self.assertEqual(len(files), 2)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     unittest.main()
