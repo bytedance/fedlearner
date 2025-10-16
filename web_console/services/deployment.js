661c71d754f84cd107fb6355183ef65669980696
lianziqt <mayuhang.123@bytedance.com>
2025-10-16 14:13:30 +0800

    delete useless codes (#1108)
    * delete web_console [ci skip]

--- web_console/services/deployment.js
+++
@@ -1,7 +0,0 @@
-import { client } from '../libs/http';
-
-export async function updateDeployment(deployment) {
-  return client.put(`deployments/${deployment.metadata.name}`, { json: deployment }).json();
-}
-
-export async function foo() { }
