ea5f1562e19eeebd0b6c7c347b7bf461051140e5
lianziqt <mayuhang.123@bytedance.com>
2025-10-16 14:13:30 +0800

    delete useless codes (#1108)
    * delete web_console [ci skip]

--- web_console/pages/datasource/job/index.jsx
+++
@@ -1,5 +0,0 @@
-import CommonJobList from '../../../components/CommonJobList'
-
-export default function JobList() {
-  return <CommonJobList datasoure></CommonJobList>
-}
--- web_console/pages/training/job/[id].jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-
-import Job from '../../../components/Job';
-
-export default function Jobs() {
-  const router = useRouter();
-  return <Job id={router.query.id}></Job>
-}
